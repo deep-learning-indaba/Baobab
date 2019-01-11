b2df869595f1ad1f8f27cddb82e75d1db2f0b432
Kale-ab <kaleabtessera@gmail.com>
2019-01-11 16:56:40 +0200

    Added basic navbar.

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
 import Home from './pages/home';
 import './App.css';
@@ -8,14 +9,18 @@ class App extends Component {
   render() {
     return (
       <Router>
-      <div className="App">
-        <header className="App-header">
-        <Link to="/">Home</Link>
-        </header>
-        <div className="Body">
+        <div >
+          <Navbar className="HomeNavBar" >
+            <Nav>
+              <NavItem eventKey={1} href="#">
+                Home
+              </NavItem>
+            </Nav>
+          </Navbar>
+          <div className="Body">
             <Route exact path="/" component={Home} />
+          </div>
         </div>
-      </div>
       </Router>
     );
   }
