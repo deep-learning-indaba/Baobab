2b8de1e4c4a807de614212bf819379a4cd3c212f
Kale-ab <kaleabtessera@gmail.com>
2019-01-11 16:19:14 +0200

    Added routing and background images.

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import Home from './pages/home';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <Link to="/">Home</Link>
         </header>
+        <div className="Body">
+            <Route exact path="/" component={Home} />
+        </div>
       </div>
+      </Router>
     );
   }
 }
