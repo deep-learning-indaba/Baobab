9ca35c8300a7b63ead1747d7e755f984defcdac7
Avishkar Bhoopchand <avishkar@Avishkars-MacBook-Pro.local>
2019-01-13 12:09:22 +0000

    Switched to Bootstrap 4, minor styling change

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
-
+import logo from './images/logo-32x32-white.png'
 import Home from './pages/home';
 import './App.css';
 
@@ -9,14 +8,24 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div >
-          <Navbar className="HomeNavBar" >
-            <Nav>
-              <NavItem eventKey={1} href="#">
-                Home
-              </NavItem>
-            </Nav>
-          </Navbar>
+        <div>
+          <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="/">
+              <img src={logo} width="30" height="30" class="d-inline-block align-top" alt="" />
+              Baobab
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item active">
+                  <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+
           <div className="Body">
             <Route exact path="/" component={Home} />
           </div>
