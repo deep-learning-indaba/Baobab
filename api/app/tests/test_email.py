af85122b5789790e260e9332c407dcd24213bc60
nischalhp <nischal.hp@gmail.com>
2019-01-19 23:12:13 +0100

    Adding tests for logger and email

---
+++ api/app/tests/test_email.py
@@ -0,0 +1,15 @@
+import unittest
+from app.utils.email import send_mail
+from app import LOGGER
+
+class TestMailer(unittest.TestCase):
+    def test_send_email(self):
+        try:
+            LOGGER.info("Testing mailer")
+            send_mail(recipient='nischal.hp@gmail.com', subject='TestSMTPEmail', body_text='Hello world from Amazon SES')
+            return True
+        except Exception:
+            return False
+
+if __name__ == '__main__':
+    unittest.main()
