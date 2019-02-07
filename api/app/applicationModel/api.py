43a385feff5e75cefd0b0db06990452c5753699f
Jean-vicente De Carvalho <carvalho96v2@gmail.com>
2019-02-07 22:26:55 +0200

    The get now returns the form, section, and questions in the appropriate JSON format.
    ToDo: add test cases and defensive coding

--- api/app/applicationModel/api.py
+++ api/app/applicationModel/api.py
@@ -17,13 +17,27 @@ from app import db, bcrypt
 
 class ApplicationFormAPI(ApplicationFormMixin, restful.Resource):
 
-    #@auth_required
+    question_fields = {
+        'id': fields.Integer,
+        'type': fields.String,
+        'description': fields.String,
+        'order': fields.Integer
+    } 
+
+    section_fields = {
+        'id': fields.Integer,
+        'name': fields.String,
+        'description': fields.String,
+        'order': fields.Integer,  
+        'questions': fields.List(fields.Nested(question_fields))   
+    }
 
     form_fields = {
         'id': fields.Integer,
         'event_id': fields.Integer,
         'is_open':  fields.Boolean,
-        'deadline': fields.DateTime
+        'deadline': fields.DateTime,
+        'sections': fields.List(fields.Nested(section_fields)) 
     }
 
     @marshal_with(form_fields)
@@ -31,7 +45,19 @@ class ApplicationFormAPI(ApplicationFormMixin, restful.Resource):
         req_parser = reqparse.RequestParser()
         req_parser.add_argument('event_id', type=int, required=True, help = 'Invalid event_id requested. Event_id\'s should be of type int.')
         args = req_parser.parse_args()
-        form = db.session.query(ApplicationForm).filter(ApplicationForm.event_id == args['event_id']).first()
+
+        form = db.session.query(ApplicationForm).filter(ApplicationForm.event_id == args['event_id']).first()     
+        sections = db.session.query(Section).filter(Section.application_form_id == form.id).all()   #All sections in our form
+        questions = db.session.query(Question).filter(Question.application_form_id == form.id).all() #All questions in our form        
+
+        form.sections = sections
+
+        for s in form.sections:
+            s.questions = []
+            for q in questions:
+                if(q.section_id == s.id):
+                    s.questions.append(q)
+
         if form: 
             return form
         else: 
