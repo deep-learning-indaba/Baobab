d790c25436a7a63fd8abc94103cceb2719e11172
Jean-vicente De Carvalho <carvalho96v2@gmail.com>
2019-02-07 23:40:42 +0200

    Added a test event to avoid foreign key integrity errors

--- api/app/applicationModel/tests.py
+++ api/app/applicationModel/tests.py
@@ -3,15 +3,21 @@ import json
 from app import db
 from app.utils.testing import ApiTestCase
 from app.applicationModel.models import ApplicationForm, Section, Question
+from app.events.models import Event
+
 
 
 class UserApiTest(ApiTestCase):
+    test_event = Event('Test Event', 'Event Description', '2019/02/24', '2019/03/24')
     test_form = ApplicationForm(1, True, '2019/03/24')
     test_section = Section(test_form.id, 'Test Section', 'Test Description', 1)
     test_question = Question(test_form.id, test_section.id, 'Test Question Description', 1, 'Test Type')
 
 
     def seed_static_data(self):
+        test_event = Event('Test Event', 'Event Description', '2019/02/24', '2019/03/24')
+        db.session.add(test_event)
+        db.session.commit()
         test_form = ApplicationForm(1, True, '2019/03/24')
         db.session.add(test_form)
         db.session.commit()
@@ -21,16 +27,17 @@ class UserApiTest(ApiTestCase):
         test_question = Question(test_form.id, test_section.id, 'Test Question Description', 1, 'Test Type')
         db.session.add(test_question)
         db.session.flush()
-        return test_form, test_section, test_question
+        return test_form, test_section, test_question, test_event
     
-    def delete_seed_data(self, form, section, question):
+    def delete_seed_data(self, form, section, question, event):
         db.delete(form)
         db.delete(section)
         db.delete(question)
+        db.delete(event)
         db.flush()
 
     def test_get_user(self):
-        test_form, test_section, test_question = self.seed_static_data()  
+        test_form, test_section, test_question, test_event = self.seed_static_data()  
 
         response = self.app.get('/api/v1/application-form?event_id=1')
         data = json.loads(response.data)
@@ -44,7 +51,7 @@ class UserApiTest(ApiTestCase):
         assert data['sections']['questions']['order'] == 1
         assert data['sections']['questions']['type'] == 'Test Type'
 
-        self.delete_seed_data(test_form, test_section, test_question)
+        self.delete_seed_data(test_form, test_section, test_question, test_event)
 
 
 
