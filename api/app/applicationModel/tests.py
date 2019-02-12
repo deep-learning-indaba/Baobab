3c09e0ebee518cd8b241d03235965c5f1547cc9a
Avishkar Bhoopchand <avishkar@google.com>
2019-02-12 21:30:20 +0000

    Updated tests to use SQLLite in-memory database and fixed some test issues this picked up

---
+++ api/app/applicationModel/tests.py
@@ -0,0 +1,45 @@
+import json
+from datetime import date
+from app import db
+from app.events.models import Event
+from app.utils.testing import ApiTestCase
+from app.applicationModel.models import ApplicationForm, Section, Question
+
+
+
+class ApplicationFormApiTest(ApiTestCase):
+
+
+    def seed_static_data(self):
+        test_event = Event('Test Event', 'Event Description', date(2019, 2, 24), date(2019, 3, 24))
+        db.session.add(test_event)
+        db.session.commit()
+        test_form = ApplicationForm(test_event.id, True, date(2019, 3, 24))
+        db.session.add(test_form)
+        db.session.commit()
+        test_section = Section(test_form.id, 'Test Section', 'Test Description', 1)
+        db.session.add(test_section)
+        db.session.commit()
+        test_question = Question(test_form.id, test_section.id, 'Test Question Description', 1, 'Test Type')
+        db.session.add(test_question)
+        db.session.flush()
+
+    def test_get_form(self):
+        self.seed_static_data()  
+
+        response = self.app.get('/api/v1/application-form?event_id=1')
+        print(response.data)
+        data = json.loads(response.data)
+        assert data['deadline'] == 'Sun, 24 Mar 2019 00:00:00 -0000'
+        assert data['event_id'] == 1
+        assert data['is_open'] == True
+        assert data['sections'][0]['description'] == 'Test Description'
+        assert data['sections'][0]['name'] == 'Test Section'
+        assert data['sections'][0]['order'] == 1
+        assert data['sections'][0]['questions'][0]['description'] == 'Test Question Description'
+        assert data['sections'][0]['questions'][0]['order'] == 1
+        assert data['sections'][0]['questions'][0]['type'] == 'Test Type'
+
+
+
+
---
+++ api/app/users/models.py
@@ -0,0 +1,106 @@
+from datetime import datetime, timedelta
+
+from app import db, bcrypt
+from app.utils.misc import make_code
+
+
+def expiration_date():
+    return datetime.now() + timedelta(days=1)
+
+
+class AppUser(db.Model):
+
+    id = db.Column(db.Integer(), primary_key=True)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    firstname = db.Column(db.String(100), nullable=False)
+    lastname = db.Column(db.String(100), nullable=False)
+    user_title = db.Column(db.String(20), nullable=False)
+    nationality_country_id = db.Column(db.Integer(), db.ForeignKey('country.id'), nullable=False)
+    residence_country_id = db.Column(db.Integer(), db.ForeignKey('country.id'), nullable=False)
+    user_ethnicity = db.Column(db.String(50), nullable=False)
+    user_gender = db.Column(db.String(20), nullable=False)
+    affiliation = db.Column(db.String(255), nullable=False)
+    department = db.Column(db.String(255), nullable=False)
+    user_disability = db.Column(db.String(255), nullable=False)
+    user_category_id = db.Column(db.Integer(), db.ForeignKey('user_category.id'), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    active = db.Column(db.Boolean(), nullable=False)
+    is_admin = db.Column(db.Boolean(), nullable=False)
+    is_deleted = db.Column(db.Boolean(), nullable=False)
+    deleted_datetime_utc = db.Column(db.DateTime(), nullable=True)
+
+    nationality_country = db.relationship('Country', foreign_keys=[nationality_country_id])
+    residence_country = db.relationship('Country', foreign_keys=[residence_country_id])
+    user_category = db.relationship('UserCategory')
+
+    def __init__(self,
+                 email,
+                 firstname,
+                 lastname,
+                 user_title,
+                 nationality_country_id,
+                 residence_country_id,
+                 user_ethnicity,
+                 user_gender,
+                 affiliation,
+                 department,
+                 user_disability,
+                 user_category_id,
+                 password,
+                 is_admin=False):
+        self.email = email
+        self.firstname = firstname
+        self.lastname = lastname
+        self.user_title = user_title
+        self.nationality_country_id = nationality_country_id
+        self.residence_country_id = residence_country_id
+        self.user_ethnicity = user_ethnicity
+        self.user_gender = user_gender
+        self.affiliation = affiliation
+        self.department = department
+        self.user_disability = user_disability
+        self.user_category_id = user_category_id
+        self.set_password(password)
+        self.active = True
+        self.is_admin = is_admin
+        self.is_deleted = False
+        self.deleted_datetime_utc = None
+
+    def set_password(self, password):
+        self.password = bcrypt.generate_password_hash(password)
+
+    def deactivate(self):
+        self.active = False
+
+
+class PasswordReset(db.Model):
+
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('app_user.id'))
+    code = db.Column(db.String(255), unique=True, default=make_code)
+    date = db.Column(db.DateTime(), default=expiration_date)
+
+    user = db.relationship(AppUser)
+
+    db.UniqueConstraint('user_id', 'code', name='uni_user_code')
+
+    def __init__(self, user):
+        self.user = user
+
+class Country(db.Model):
+    def __init__(self, name):
+        self.name = name
+
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+class UserCategory(db.Model):
+    def __init__(self, name, description=None, group=None):
+        self.name = name
+        self.description = description
+        self.group = group
+
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(500))
+    group = db.Column(db.String(100))
