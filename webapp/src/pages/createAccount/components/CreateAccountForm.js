bb7b4814d2807ccf86ecf778450cc448ac9afc46
Kale-ab <kaleabtessera@gmail.com>
2019-02-16 15:02:38 +0200

    Added basic responsiveness to Create Account form.

--- webapp/src/pages/createAccount/components/CreateAccountForm.js
+++ webapp/src/pages/createAccount/components/CreateAccountForm.js
@@ -10,6 +10,7 @@ import {
   getCounties,
   genderOptions
 } from "../../../utils/validation/contentHelpers";
+import { createColClassName } from "../../../utils/styling/styling";
 
 class CreateAccountForm extends Component {
   constructor(props) {
@@ -59,6 +60,13 @@ class CreateAccountForm extends Component {
   };
 
   render() {
+    const xs = 12;
+    const sm = 6;
+    const md = 6;
+    const lg = 6;
+    const commonColClassName = createColClassName(xs, sm, md, lg);
+    const colClassNameTitle = createColClassName(12, 4, 2, 2);
+    const colClassNameSurname = createColClassName(12, 4, 5, 5);
     const {
       firstName,
       lastName,
@@ -79,32 +87,40 @@ class CreateAccountForm extends Component {
       disability
     } = this.state;
     return (
-      <div className="Login">
+      <div className="CreateAccount">
         <form onSubmit={this.handleSubmit}>
-          <FormSelect
-            options={titleOptions}
-            id={validationFields.title.name}
-            placeholder={validationFields.title.display}
-            onChange={this.handleChange(validationFields.title)}
-            value={title}
-            label={validationFields.title.display}
-          />
-          <FormTextBox
-            id={validationFields.firstName.name}
-            type="text"
-            placeholder={validationFields.firstName.display}
-            onChange={this.handleChange(validationFields.firstName)}
-            value={firstName}
-            label={validationFields.firstName.display}
-          />
-          <FormTextBox
-            id={validationFields.lastName.name}
-            type="text"
-            placeholder={validationFields.lastName.display}
-            onChange={this.handleChange(validationFields.lastName)}
-            value={lastName}
-            label={validationFields.lastName.display}
-          />
+          <div class="row">
+            <div class={colClassNameTitle}>
+              <FormSelect
+                options={titleOptions}
+                id={validationFields.title.name}
+                placeholder={validationFields.title.display}
+                onChange={this.handleChange(validationFields.title)}
+                value={title}
+                label={validationFields.title.display}
+              />
+            </div>
+            <div class={colClassNameSurname}>
+              <FormTextBox
+                id={validationFields.firstName.name}
+                type="text"
+                placeholder={validationFields.firstName.display}
+                onChange={this.handleChange(validationFields.firstName)}
+                value={firstName}
+                label={validationFields.firstName.display}
+              />
+            </div>
+            <div class={colClassNameSurname}>
+              <FormTextBox
+                id={validationFields.lastName.name}
+                type="text"
+                placeholder={validationFields.lastName.display}
+                onChange={this.handleChange(validationFields.lastName)}
+                value={lastName}
+                label={validationFields.lastName.display}
+              />
+            </div>
+          </div>
           <FormTextBox
             id={validationFields.email.name}
             type="email"
@@ -113,54 +129,72 @@ class CreateAccountForm extends Component {
             value={email}
             label={validationFields.email.display}
           />
-          <FormSelect
-            options={getCounties()}
-            id={validationFields.nationality.name}
-            placeholder={validationFields.nationality.display}
-            onChange={this.handleChange(validationFields.nationality)}
-            value={nationality}
-            label={validationFields.nationality.display}
-          />
-          <FormSelect
-            options={getCounties()}
-            id={validationFields.residence.name}
-            placeholder={validationFields.residence.display}
-            onChange={this.handleChange(validationFields.residence)}
-            value={residence}
-            label={validationFields.residence.display}
-          />
-          <FormTextBox
-            id={validationFields.ethnicity.name}
-            type="text"
-            placeholder={validationFields.ethnicity.display}
-            onChange={this.handleChange(validationFields.ethnicity)}
-            value={ethnicity}
-            label={validationFields.ethnicity.display}
-          />
-          <FormSelect
-            options={genderOptions}
-            id={validationFields.gender.name}
-            placeholder={validationFields.gender.display}
-            onChange={this.handleChange(validationFields.gender)}
-            value={gender}
-            label={validationFields.gender.display}
-          />
-          <FormTextBox
-            id={validationFields.affiliation.name}
-            type="text"
-            placeholder={validationFields.affiliation.display}
-            onChange={this.handleChange(validationFields.affiliation)}
-            value={affiliation}
-            label={validationFields.affiliation.display}
-          />
-          <FormTextBox
-            id={validationFields.department.name}
-            type="text"
-            placeholder={validationFields.department.display}
-            onChange={this.handleChange(validationFields.department)}
-            value={department}
-            label={validationFields.department.display}
-          />
+          <div class="row">
+            <div class={commonColClassName}>
+              <FormSelect
+                options={getCounties()}
+                id={validationFields.nationality.name}
+                placeholder={validationFields.nationality.display}
+                onChange={this.handleChange(validationFields.nationality)}
+                value={nationality}
+                label={validationFields.nationality.display}
+              />
+            </div>
+            <div class={commonColClassName}>
+              <FormSelect
+                options={getCounties()}
+                id={validationFields.residence.name}
+                placeholder={validationFields.residence.display}
+                onChange={this.handleChange(validationFields.residence)}
+                value={residence}
+                label={validationFields.residence.display}
+              />
+            </div>
+          </div>
+          <div class="row">
+            <div class={commonColClassName}>
+              <FormTextBox
+                id={validationFields.ethnicity.name}
+                type="text"
+                placeholder={validationFields.ethnicity.display}
+                onChange={this.handleChange(validationFields.ethnicity)}
+                value={ethnicity}
+                label={validationFields.ethnicity.display}
+              />
+            </div>
+            <div class={commonColClassName}>
+              <FormSelect
+                options={genderOptions}
+                id={validationFields.gender.name}
+                placeholder={validationFields.gender.display}
+                onChange={this.handleChange(validationFields.gender)}
+                value={gender}
+                label={validationFields.gender.display}
+              />
+            </div>
+          </div>
+          <div class="row">
+            <div class={commonColClassName}>
+              <FormTextBox
+                id={validationFields.affiliation.name}
+                type="text"
+                placeholder={validationFields.affiliation.display}
+                onChange={this.handleChange(validationFields.affiliation)}
+                value={affiliation}
+                label={validationFields.affiliation.display}
+              />
+            </div>
+            <div class={commonColClassName}>
+              <FormTextBox
+                id={validationFields.department.name}
+                type="text"
+                placeholder={validationFields.department.display}
+                onChange={this.handleChange(validationFields.department)}
+                value={department}
+                label={validationFields.department.display}
+              />
+            </div>
+          </div>
           <FormTextBox
             id={validationFields.disability.name}
             type="text"
@@ -169,28 +203,34 @@ class CreateAccountForm extends Component {
             value={disability}
             label={validationFields.disability.display}
           />
-          <FormTextBox
-            id={validationFields.password.name}
-            type="password"
-            placeholder={validationFields.password.display}
-            onChange={this.handleChange(validationFields.password)}
-            value={password}
-            label={validationFields.password.display}
-          />
-          <FormTextBox
-            id={validationFields.confirmPassword.name}
-            type="password"
-            placeholder={validationFields.confirmPassword.display}
-            onChange={this.handleChange(validationFields.confirmPassword)}
-            value={confirmPassword}
-            label={validationFields.confirmPassword.display}
-          />
+          <div class="row">
+            <div class={commonColClassName}>
+              <FormTextBox
+                id={validationFields.password.name}
+                type="password"
+                placeholder={validationFields.password.display}
+                onChange={this.handleChange(validationFields.password)}
+                value={password}
+                label={validationFields.password.display}
+              />
+            </div>
+            <div class={commonColClassName}>
+              <FormTextBox
+                id={validationFields.confirmPassword.name}
+                type="password"
+                placeholder={validationFields.confirmPassword.display}
+                onChange={this.handleChange(validationFields.confirmPassword)}
+                value={confirmPassword}
+                label={validationFields.confirmPassword.display}
+              />
+            </div>
+          </div>
           <button
             type="submit"
             class="btn btn-primary"
             disabled={!this.validateForm() || loading}
           >
-            Login
+            Create Account
           </button>
           {loading && (
             <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
---
+++ webapp/src/utils/styling/styling.js
@@ -0,0 +1,4 @@
+export const createColClassName = (xs, sm, md, lg) => {
+  let colClassName = `col-xs-${xs}  col-sm-${sm} col-md-${md} col-lg-${lg}`;
+  return colClassName;
+};
