ca2dd6027354583ddc67f13876943628ad29786e
Avishkar Bhoopchand <avishkar@google.com>
2019-03-01 16:17:19 +0000

    Added validation_text field to question

---
+++ api/migrations/versions/35cdc3c51184_add_validation_text.py
@@ -0,0 +1,44 @@
+"""Add validation text to Question model.
+
+Revision ID: 35cdc3c51184
+Revises: d4a62bfc5be5
+Create Date: 2019-03-01 16:08:31.563227
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '35cdc3c51184'
+down_revision = 'd4a62bfc5be5'
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy import orm
+from app.applicationModel.models import Question, Section
+import logging
+
+
+def upgrade():
+    op.add_column('question', sa.Column('validation_text', sa.String(), nullable=True))
+    update_question_data()
+
+
+def update_question(session, question_id, validation_text):
+    question = session.query(Question).filter(Question.id == question_id).first()
+    question.validation_text = validation_text
+
+
+def update_question_data():
+    bind = op.get_bind()
+    session = orm.Session(bind=bind)
+    update_question(session, 1, 'Enter 50 to 150 words')
+
+    update_question(session, 2, 'Enter 50 to 150 words')
+    update_question(session, 3, 'Enter up to 80 words')
+    update_question(session, 4, 'Enter up to 80 words')
+    update_question(session, 5, 'Enter up to 150 words')
+    update_question(session, 9, 'Enter up to 150 words')
+    update_question(session, 16, 'Enter up to 150 words')
+    update_question(session, 18, 'Enter up to 150 words')
+
+def downgrade():
+    op.drop_column('question', 'validation_text')
