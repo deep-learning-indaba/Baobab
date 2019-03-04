aa1aefb13fa0aaa2216ed0ed8944e5e18bff0d51
Avishkar Bhoopchand <avishkar@google.com>
2019-03-04 22:10:15 +0000

    Updated password reset email

--- api/app/users/api.py
+++ api/app/users/api.py
@@ -199,7 +199,16 @@ class PasswordResetRequestAPI(restful.Resource):
 
         send_mail(recipient=args['email'],
                   subject='Password Reset for Deep Learning Indaba portal',
-                  body_text='Dear user, Please use the following link to successfully reset your password : {}/ResetPassword?resetToken={}'.format(BOABAB_HOST, password_reset.code))
+                  body_text="""Dear {} {} {}, 
+                  
+                  You recently requested a password reset on Baobab, please use the following link to reset you password: 
+                  {}/ResetPassword?resetToken={}
+                  
+                  If you did not request a password reset, please ignore this email and contact the Deep Learning Indaba organisers.
+
+                  Kind Regards,
+                  The Baobab Team
+                  """.format(user.user_title, user.firstname, user.lastname, BOABAB_HOST, password_reset.code))
 
         return {}, 201
 
