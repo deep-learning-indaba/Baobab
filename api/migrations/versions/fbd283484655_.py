e52298109df73f81b1cb2c440a4e4cc0d94e3242
Jade Abbott <jabbott@retrorabbit.co.za>
2019-03-05 09:30:00 +0200

    You need to freeze the model within the migration when doing data migrations

--- api/migrations/versions/fbd283484655_.py
+++ api/migrations/versions/fbd283484655_.py
@@ -13,7 +13,53 @@ down_revision = '6fa47605c615'
 from alembic import op
 import sqlalchemy as sa
 from sqlalchemy import orm
-from app.applicationModel.models import Question, Section
+from app import db, bcrypt
+
+
+from sqlalchemy.ext.declarative import declarative_base, DeferredReflection
+
+Base = declarative_base()
+
+class Section(Base):
+    __tablename__ = 'section'
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer(), primary_key=True)
+    application_form_id = db.Column(db.Integer(), db.ForeignKey('application_form.id'), nullable=False)
+    name = db.Column(db.String(255), unique=True, nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    order = db.Column(db.Integer(), unique=True, nullable=False)
+
+    def __init__(self, application_form_id, name, description, order):
+        self.application_form_id = application_form_id
+        self.name = name
+        self.description = description
+        self.order = order
+
+
+class Question(Base):
+    __tablename__ = 'question'
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer(), primary_key=True)
+    section_id = db.Column(db.Integer(), db.ForeignKey('section.id'), nullable=False)
+    type = db.Column(db.String(), nullable=False)
+    description = db.Column(db.String(), nullable=True)
+    headline = db.Column(db.String(), nullable=False)
+    placeholder = db.Column(db.String(), nullable=True)
+    validation_regex = db.Column(db.String(), nullable=True)
+    order = db.Column(db.Integer(), nullable=False)
+    options = db.Column(db.JSON(), nullable=True)
+    is_required = db.Column(db.Boolean(), nullable=False)
+
+    def __init__(self, application_form_id, section_id, headline, placeholder, order, questionType, validation_regex, is_required = True, description = None, options = None):
+        self.section_id = section_id
+        self.headline = headline
+        self.placeholder = placeholder
+        self.order = order
+        self.type = questionType
+        self.description = description
+        self.options = options
+        self.is_required = is_required
+        self.validation_regex = validation_regex
 
 
 
@@ -32,6 +78,7 @@ def update_question(session, question_id, placeholder, validation_regex):
     question.validation_regex = validation_regex
 
 def update_question_data():
+    Base.metadata.bind = op.get_bind()
     bind = op.get_bind()
     session = orm.Session(bind=bind)
     update_question(session, 1, 'Enter 50 to 150 words', r'^\W*(\w+(\W+|$)){50,150}$')
