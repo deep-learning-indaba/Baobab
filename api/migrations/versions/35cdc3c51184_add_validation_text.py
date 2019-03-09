698b3cd41908f86379a5a1d69b41b9c5e7465a7b
Jade Abbott <jabbott@retrorabbit.co.za>
2019-03-09 21:50:04 +0200

    Updated migration to use freezed model

--- api/migrations/versions/35cdc3c51184_add_validation_text.py
+++ api/migrations/versions/35cdc3c51184_add_validation_text.py
@@ -13,10 +13,45 @@ down_revision = 'd4a62bfc5be5'
 from alembic import op
 import sqlalchemy as sa
 from sqlalchemy import orm
-from app.applicationModel.models import Question, Section
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import orm
+from app import db
+
+
+Base = declarative_base()
+
+
+class Question(Base):
+    __tablename__ = 'question'
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer(), primary_key=True)
+    application_form_id = db.Column(db.Integer(), db.ForeignKey('application_form.id'), nullable=False)
+    section_id = db.Column(db.Integer(), db.ForeignKey('section.id'), nullable=False)
+    type = db.Column(db.String(), nullable=False)
+    description = db.Column(db.String(), nullable=True)
+    headline = db.Column(db.String(), nullable=False)
+    placeholder = db.Column(db.String(), nullable=True)
+    validation_regex = db.Column(db.String(), nullable=True)
+    order = db.Column(db.Integer(), nullable=False)
+    options = db.Column(db.JSON(), nullable=True)
+    is_required = db.Column(db.Boolean(), nullable=False)
+
+    def __init__(self, application_form_id, section_id, headline, placeholder, order, questionType, validation_regex, is_required = True, description = None, options = None):
+        self.application_form_id = application_form_id
+        self.section_id = section_id
+        self.headline = headline
+        self.placeholder = placeholder
+        self.order = order
+        self.type = questionType
+        self.description = description
+        self.options = options
+        self.is_required = is_required
+        self.validation_regex = validation_regex
 
 
 def upgrade():
+    Base.metadata.bind = op.get_bind()
+    session = orm.Session(bind=Base.metadata.bind)
     op.add_column('question', sa.Column('validation_text', sa.String(), nullable=True))
     update_question_data()
 
