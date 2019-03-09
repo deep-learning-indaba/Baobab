1164d5b5560e2629b737c969a8021b36ba5688c8
Jade Abbott <jabbott@retrorabbit.co.za>
2019-03-09 23:09:32 +0200

    Added a basic admin dashboard

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -7,9 +7,12 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from flask_redis import FlaskRedis
 from utils.logger import Logger
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 app = Flask(__name__)
 app.config.from_object('config')
+
 CORS(app, resources={r"/api/*": {"origins": "*"}}, supports_credentials=True)
 print(app.config['SQLALCHEMY_DATABASE_URI'])
 rest_api = restful.Api(app)
@@ -25,3 +28,23 @@ migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+# set optional bootswatch theme
+app.config['FLASK_ADMIN_SWATCH'] = 'cosmo'
+from applicationModel.models import Question, Section
+from responses.models import Response, Answer
+from users.models import UserCategory, AppUser
+from events.models import Event, EventRole
+admin = Admin(app, name='Deep Learning Indaba Admin Portal')
+
+
+admin.add_view(ModelView(Question, db.session))
+admin.add_view(ModelView(Section, db.session))
+admin.add_view(ModelView(Response, db.session))
+admin.add_view(ModelView(Answer, db.session))
+
+admin.add_view(ModelView(Event, db.session))
+admin.add_view(ModelView(EventRole, db.session))
+admin.add_view(ModelView(UserCategory, db.session))
+
+
