4e4bc1528c30112e5bde35c627474c69d1636f91
Kale-ab <kaleabtessera@gmail.com>
2019-03-09 15:40:27 +0200

    Added basic support for IE9 and up.

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-require('dotenv').config()
+import "react-app-polyfill/ie9";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+require("dotenv").config();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
