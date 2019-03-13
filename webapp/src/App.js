d7b18ac2ed3be40187376fe63ccb50e5d272c736
Kale-ab Tessera <kale-abt@discovery.co.za>
2019-03-13 09:14:30 +0200

    Fixed Indaba link.

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -166,7 +166,7 @@ class App extends Component {
             <div class="container">
               <p>
                 Baobab, © 2019 |{" "}
-                <a href="www.deeplearningindaba.com">Deep Learning Indaba</a>
+                <a href="https://www.deeplearningindaba.com">Deep Learning Indaba</a>
               </p>
             </div>
           </footer>
