68ddf59fdbbaa0c0f7447912725e3aeb08b50eac
Kale-ab Tessera <kale-abt@discovery.co.za>
2019-03-27 13:39:12 +0200

    Added interceptor for failed auth reqs.

--- webapp/src/services/base.service.js
+++ webapp/src/services/base.service.js
@@ -1,10 +1,26 @@
+import axios from "axios";
+
 export function authHeader() {
-    // return authorization header with basic auth credentials
-    let user = JSON.parse(localStorage.getItem("user"));
-  
-    if (user) {
-      return { "Authorization": user.token };
-    } else {
-      return {};
+  // return authorization header with basic auth credentials
+  let user = JSON.parse(localStorage.getItem("user"));
+
+  if (user) {
+    return { Authorization: user.token };
+  } else {
+    return {};
+  }
+}
+
+function errorResponseHandler(error) {
+  if (error.response) {
+    console.log(error.response.status);
+    if (error.response.status === 401) {
+      window.location = "/login";
     }
   }
+}
+
+// apply interceptor on response
+axios.interceptors.response.use(response => response, errorResponseHandler);
+
+export default errorResponseHandler;
