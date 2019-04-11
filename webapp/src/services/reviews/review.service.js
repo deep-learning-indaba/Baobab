436130e028d5efcdd444347f5421581d292743d4
Jade Abbott <jabbott@retrorabbit.co.za>
2019-04-11 21:50:35 +0200

    added services for review assignment

--- webapp/src/services/reviews/review.service.js
+++ webapp/src/services/reviews/review.service.js
@@ -5,7 +5,9 @@ const baseUrl = process.env.REACT_APP_API_URL;
 
 export const reviewService = {
     getReviewForm,
-    submit
+    submit,
+    getReviewAssignments,
+    assignReviews
 }
 
 function getReviewForm(eventId, skip) {
@@ -44,4 +46,57 @@ function submit(responseId, reviewFormId, scores) {
                 error: (error.response && error.response.data) ? error.response.data.message : error.message
             };
         });
+}
+
+function getReviewAssignments(eventId) {
+    return axios
+        .get(baseUrl + "/api/v1/reviewassignment?event_id=" + eventId, { 'headers': authHeader() })
+        .then(function(response) {
+            return {
+                reviewers: response.data,
+                error: ""
+            };
+        })
+        .catch(function(error) {
+            return {
+                reviewers: null,
+                error: (error.response && error.response.data) ? error.response.data.message : error.message
+            };
+        });    
+}
+
+function assignReviews(eventId, reviewers) {
+	Promise.all(reviewers.map(review => assignReview(eventId, review.email, review.numNewReviews)))
+	.then(function(response) {
+		return {
+			error: ""
+		}
+	})
+	.catch(function(error) {
+		//TODO: This probably won't work as expected with Promise.all
+		return {
+			error: (error.response && error.response.data) ? error.response.data.message : error.message
+		};
+	});
+}
+
+function assignReview(eventId, reviewerUserEmail, numReviews) {
+    let assignment = {
+        "event_id": eventId,
+        "reviewer_user_email": reviewerUserEmail,
+        "num_reviews": numReviews
+    };
+
+    return axios
+        .post(baseUrl + "/api/v1/reviewassignment", assignment, { 'headers': authHeader() })
+        .then(function(response) {
+            return {
+                error: ""
+            }
+        })
+        .catch(function(error) {
+            return {
+                error: (error.response && error.response.data) ? error.response.data.message : error.message
+            };
+        });    
 }
