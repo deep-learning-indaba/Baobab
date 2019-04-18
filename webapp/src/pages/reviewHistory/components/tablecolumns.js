24128daba6776677c5101384f12c73a20a66258a
Avishkar Bhoopchand <avishkar@google.com>
2019-04-18 17:03:54 +0100

    Updated link to be an edit button in table

--- webapp/src/pages/reviewHistory/components/tablecolumns.js
+++ webapp/src/pages/reviewHistory/components/tablecolumns.js
@@ -1,7 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
 export const columns = [
   {
-    Header: "Review Response ID",
+    Header: " ",
     accessor: "review_response_id",
+    Cell: row => {
+      return <Link to={"/review/" + row.row.review_response_id}><i className="fa fa-edit"></i></Link>
+    },
     filterable: false
   },
   {
