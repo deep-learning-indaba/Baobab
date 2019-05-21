478d509cc616b6eabff400d45c9d3b4d313d3ffd
Avishkar Bhoopchand <avishkar@google.com>
2019-05-21 23:28:34 +0100

    Updated homepage with application status for completion campaign

--- webapp/src/pages/home/Home.js
+++ webapp/src/pages/home/Home.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import logo from '../../images/indaba-logo-dark.png';
 import './Home.css';
 import { getEvents } from "../../services/events";
+import { applicationFormService } from "../../services/applicationForm";
 import { NavLink } from "react-router-dom";
+
+const DEFAULT_EVENT_ID = process.env.REACT_APP_DEFAULT_EVENT_ID || 1;
 
 const headings = ["Event", "Start date", "End date", "Status"];
 const fieldNames = ["description", "start_date", "end_date", "status"];
@@ -15,17 +18,42 @@ class Home extends Component {
 
     this.state = {
       headings: headings,
-      rows: []
+      rows: [],
+      applicationStatus: null
     }
   }
 
   componentDidMount() {
-    getEvents().then(response => {
-      if (response) {
+    // Temporarily removed during campaign to get people to complete their applications
+    // getEvents().then(response => {
+    //   if (response) {
+    //     this.setState({
+    //       headings: headings,
+    //       rows: response
+    //     })
+    //   }
+    // });
+
+    applicationFormService.getResponse(DEFAULT_EVENT_ID).then(resp => {
+      let applicationStatus = null;
+      if (resp.response) {
+        if (resp.response.is_submitted) {
+          applicationStatus = "Submitted";
+        }
+        else if (resp.response.is_withdrawn) {
+          applicationStatus = "Withdrawn";
+        }
+        else {
+          applicationStatus = "NOT Submitted";
+        }
         this.setState({
-          headings: headings,
-          rows: response
+          applicationStatus: applicationStatus
         })
+      }
+      else {
+        this.setState({
+          applicationStatus: "Not Started"
+        });
       }
     });
   }
@@ -78,10 +106,41 @@ class Home extends Component {
         <div>
           <img src={logo} className="img-fluid" alt="logo" />
         </div>
-        <h2 className="Blurb">Welcome to Baobab</h2>
+        {this.props.user && this.state.applicationStatus && <h3 className="Blurb">Application Status for Deep Learning Indaba 2019</h3>}
+        {!this.props.user && <h2 className="Blurb">Welcome to Baobab</h2>}
+
+        {(this.props.user && this.state.applicationStatus) && 
+          <div>
+            <div class="status2019 row text-center">
+              <div className="col-sm">
+                <h4 className={this.state.applicationStatus == "Submitted" ? "text-success" : "text-danger"}>
+                  {this.state.applicationStatus}
+                </h4>
+                
+                {this.state.applicationStatus == "Submitted" && <p>
+                  Thank you! Your application has been received and is under review. You can expect to hear our decision mid-June.
+                </p>}
+                {this.state.applicationStatus == "Withdrawn" && <p>
+                  Your application has been withdrawn - you will not be considered for a place at the Indaba.
+                </p>}
+                {this.state.applicationStatus == "NOT Submitted" && <p>
+                  You have NOT submitted your application! <NavLink to="/applicationForm">Complete and submit</NavLink> it before 26 May to be considered for a place at the Indaba.
+                </p>}
+                {this.state.applicationStatus == "Not Started" && <p>
+                  We have not recieved an application from you. <NavLink to="/applicationForm">Complete it</NavLink> before 26 May to be considered for a place at the Indaba.
+                </p>}
+                
+              </div>
+            </div>
+          </div>
+        }
+
         {!this.props.user && 
           <p class="text-center"><NavLink to="/createAccount">Sign up</NavLink> for an account in order to apply for an Indaba event, or <NavLink to="/login">login</NavLink> if you already have one.</p>}
-        {this.props.user && table}
+        {
+          //Temporarily removed during campaign to get people to complete their applications
+          //this.props.user && table
+        }
       </div>
     );
   }
