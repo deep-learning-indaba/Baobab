2823fc99507c0cfdf11d5eeadce42abbce988f6f
ilzeLourens <ilourens@retrorabbit.co.za>
2019-05-29 11:25:50 +0200

    added error message for when email is already used

--- webapp/src/services/invitedGuests/invitedGuests.service.js
+++ webapp/src/services/invitedGuests/invitedGuests.service.js
@@ -4,86 +4,95 @@ import { authHeader } from '../base.service';
 const baseUrl = process.env.REACT_APP_API_URL;
 
 export const invitedGuestServices = {
-    getInvitedGuestList,
-    addInvitedGuest,
-    createInvitedGuest
+  getInvitedGuestList,
+  addInvitedGuest,
+  createInvitedGuest
 }
 
 function getInvitedGuestList(eventId) {
-    return axios
-      .get(baseUrl + "/api/v1/invitedGuestList?event_id=" + eventId , {
-        headers: authHeader()
-      })
-      .then(function(response) {
-        return {
-          form: response.data,
-          error: ""
-        };
-      })
-      .catch(function(error) {
-        return {
-          form: null,
-          error:
-            error.response && error.response.data
-              ? error.response.data.message
-              : error.message
-        };
-      });
-  }
-
-  function addInvitedGuest(email_address, event_Id, role) {
-    let data = {
-      event_id: event_Id,
-      email_address : email_address,
-      role: role,
-    };
-    return axios
-        .post(baseUrl + "/api/v1/invitedGuest", data, { headers: authHeader() })
-        .then(function(response) {
-            return {
-              msg: "succeeded",
-              response: response
-            }
-        })
-        .catch(function(error) {
-          if(error.response.status === 404){
-            console.log(error.response.status)
-            return {msg: "404"}
-          }
-          else{
-            return {
-                msg: "Failed",
-                error: (error.response && error.response.data) ? error.response.data.message : error.message,
-            }
-          }
-        })
+  return axios
+    .get(baseUrl + "/api/v1/invitedGuestList?event_id=" + eventId, {
+      headers: authHeader()
+    })
+    .then(function (response) {
+      return {
+        form: response.data,
+        error: ""
+      };
+    })
+    .catch(function (error) {
+      return {
+        form: null,
+        error:
+          error.response && error.response.data
+            ? error.response.data.message
+            : error.message
+      };
+    });
 }
 
-
-function createInvitedGuest(email_address, event_Id, role) {
+function addInvitedGuest(email_address, event_Id, role) {
   let data = {
     event_id: event_Id,
-    email_address : email_address,
+    email_address: email_address,
     role: role,
   };
   return axios
-      .post(baseUrl + "/api/v1/invitedGuest/create", data, { headers: authHeader() })
-      .then(function(response) {
-          return {
-            msg: "succeeded",
-            response: response
-          }
-      })
-      .catch(function(error) {
-        if(error.response.status === 404){
-          console.log(error.response.status)
-          return {msg: "404"}
+    .post(baseUrl + "/api/v1/invitedGuest", data, { headers: authHeader() })
+    .then(function (response) {
+      return {
+        msg: "succeeded",
+        response: response
+      }
+    })
+    .catch(function (error) {
+      if (error.response.status === 404) {
+        return { msg: "404" }
+      }
+      else if(error.response.status === 409){
+        return { msg: "409" }
+      }
+      else {
+        return {
+          msg: "Failed",
+          error: (error.response && error.response.data) ? error.response.data.message : error.message,
         }
-        else{
-          return {
-              msg: "Failed",
-              error: (error.response && error.response.data) ? error.response.data.message : error.message,
-          }
+      }
+    })
+}
+
+
+function createInvitedGuest(user, event_Id, role) {
+  let data = {
+    event_id: event_Id,
+    email: user.email,
+    firstname: user.firstName,
+    lastname: user.lastName,
+    user_title: user.title,
+    nationality_country_id: user.nationality,
+    residence_country_id: user.residence,
+    user_gender: user.gender,
+    affiliation: user.affiliation,
+    department: user.department,
+    user_disability: user.disability,
+    user_category_id: user.category,
+    user_primaryLanguage: user.primaryLanguage,
+    user_dateOfBirth: new Date(user.dateOfBirth).toISOString(),
+    role: user.role,
+  };
+  return axios
+    .post(baseUrl + "/api/v1/invitedGuest/create", data, { headers: authHeader() })
+    .then(function (response) {
+
+      return {
+        msg: "succeeded",
+        response: response
+      }
+    })
+    .catch(function (error) {
+        return {
+          msg: "Failed",
+          error: (error.response && error.response.data) ? error.response.data.message : error.message,
         }
-      })
+    })
 }
