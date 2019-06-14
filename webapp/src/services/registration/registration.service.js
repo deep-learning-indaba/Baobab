b4a493c2464e8be346d49500cabfffbc2d403f6e
ilzeLourens <ilourens@retrorabbit.co.za>
2019-06-14 15:00:50 +0200

    removed getOffer from registration service since this service already exists in offer service

--- webapp/src/services/registration/registration.service.js
+++ webapp/src/services/registration/registration.service.js
@@ -6,32 +6,8 @@ const baseUrl = process.env.REACT_APP_API_URL;
 export const registrationService = {
   getRegistrationForm,
   getRegistrationResponse,
-  submitResponse,
-  getOffer
+  submitResponse
 };
-
-function getOffer(eventId) {
-  return axios
-    .get(baseUrl + "/api/v1/offer?event_id=" + eventId, {
-      headers: authHeader()
-    })
-    .then(function(response) {
-      return {
-        form: response.data,
-        error: ""
-      };
-    })
-    .catch(function(error) {
-      return {
-        form: null,
-        error:
-          error.response && error.response.data
-            ? error.response.data.message
-            : error.message
-      };
-    });
-}
-
 
 function getRegistrationForm(eventId, offerId) {
   return axios
