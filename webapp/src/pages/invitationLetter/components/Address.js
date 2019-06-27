46b27e8758e6aff0f5a9584874b647a716c3eb0f
Kale-ab <Kaleab.Tessera@multichoice.co.za>
2019-06-27 11:52:26 +0200

    Created address component.

---
+++ webapp/src/pages/invitationLetter/components/Address.js
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+import FormTextBox from "../../../components/form/FormTextBox";
+import { createColClassName } from "../../../utils/styling/styling";
+import validationFields from "../../../utils/validation/validationFields";
+class Address extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      streetAddress1: props.streetAddress1,
+      streetAddress2: props.streetAddress2,
+      city: props.city,
+      postalCode: props.postalCode,
+      addressText: {
+        streetAddress1Value: props.streetAddress1Value,
+        streetAddress2Value: props.streetAddress2Value,
+        cityValue: props.cityValue,
+        postalCodeValue: props.postalCodeValue
+      }
+    };
+  }
+  render() {
+    const { streetAddress1, streetAddress2, city, postalCode } = this.state;
+    const {
+      streetAddress1Value,
+      streetAddress2Value,
+      cityValue,
+      postalCodeValue
+    } = this.state.addressText;
+    const addressStyle = createColClassName(12, 4, 6, 6);
+    return (
+      <div class={addressStyle}>
+        <div>
+          <FormTextBox
+            id={streetAddress1.name}
+            type="text"
+            placeholder={streetAddress1.display}
+            onChange={this.props.onChange(streetAddress1)}
+            value={streetAddress1Value}
+            label={streetAddress1.display}
+          />
+        </div>
+        <div>
+          <FormTextBox
+            id={streetAddress2.name}
+            type="text"
+            placeholder={streetAddress2.display}
+            onChange={this.props.onChange(streetAddress2)}
+            value={streetAddress2Value}
+            label={streetAddress2.display}
+          />
+        </div>
+        <div>
+          <FormTextBox
+            id={city.name}
+            type="text"
+            placeholder={city.display}
+            onChange={this.props.onChange(city)}
+            value={cityValue}
+            label={city.display}
+          />
+        </div>
+        <div>
+          <FormTextBox
+            id={postalCode.name}
+            type="text"
+            placeholder={postalCode.display}
+            onChange={this.props.onChange(postalCode)}
+            value={postalCodeValue}
+            label={postalCode.display}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Address;
