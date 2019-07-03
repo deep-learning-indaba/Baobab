88da593b0520b170f13c6fc8c58c29dd9278138e
Kale-ab <kaleabtessera@gmail.com>
2019-07-04 01:05:16 +0200

    Reworked invitation letter backend.

--- api/app/invitationletter/api.py
+++ api/app/invitationletter/api.py
@@ -4,7 +4,7 @@ from sqlalchemy.exc import IntegrityError
 from app.utils.auth import verify_token
 from flask import g, request
 from app.invitationletter.models import InvitationTemplate
-from app.registration.models import Offer, Registration, RegistrationAnswer, RegistrationQuestion
+from app.registration.models import Offer, Registration, RegistrationAnswer, RegistrationQuestion, Registration, RegistrationForm
 from app.invitationletter.mixins import InvitationMixin
 from app.invitationletter.models import InvitationLetterRequest
 from app.invitationletter.generator import generate
@@ -12,6 +12,8 @@ from app.users.models import AppUser, Country
 from app import db, LOGGER
 from app.utils import errors
 from app.utils.auth import auth_required
+from app.events.repository import EventRepository
+from app.invitedGuest.models import GuestRegistration
 
 
 def invitation_info(invitation_request):
@@ -25,7 +27,6 @@ class InvitationLetterAPI(InvitationMixin, restful.Resource):
     @auth_required
     def post(self):
         args = self.req_parser.parse_args()
-        registration_id = args['registration_id']
         event_id = args['event_id']
         work_address = args['work_address']
         addressed_to = args['addressed_to']
@@ -33,16 +34,39 @@ class InvitationLetterAPI(InvitationMixin, restful.Resource):
         passport_name = args['passport_name']
         passport_no = args['passport_no']
         passport_issued_by = args['passport_issued_by']
-        passport_expiry_date = datetime.strptime((args['passport_expiry_date']),
-                                                 '%Y-%m-%dT%H:%M:%S.%fZ').strftime("%Y-%m-%d")
-        to_date = datetime.strptime((args['to_date']),
-                                    '%Y-%m-%dT%H:%M:%S.%fZ').strftime("%Y-%m-%d")
-        from_date = datetime.strptime((args['from_date']),
-                                      '%Y-%m-%dT%H:%M:%S.%fZ').strftime("%Y-%m-%d")
-        user_id = verify_token(request.headers.get('Authorization'))['id']
+        passport_expiry_date = args['passport_expiry_date']
 
+        registration_event = EventRepository.get_by_id(event_id)
+        if(registration_event is not None):
+            to_date = registration_event.end_date.strftime("%Y-%m-%d")
+            from_date = registration_event.start_date.strftime("%Y-%m-%d")
+        else:
+            return errors.EVENT_ID_NOT_FOUND
+
+        # Finding registation_id for this user at this event
+        user_id = verify_token(request.headers.get('Authorization'))['id']
+        offer = db.session.query(Offer).filter(
+            Offer.user_id == user_id).filter(Offer.event_id == event_id).first()
+
+        if not offer:
+            # Check if Guest Registration
+            registration = None
+            registration_form = db.session.query(RegistrationForm).filter(
+                RegistrationForm.event_id == event_id).first()
+            if(registration_form):
+                registration = db.session.query(GuestRegistration).filter(
+                    GuestRegistration.user_id == user_id).filter(GuestRegistration.registration_form_id == registration_form.id).first()
+
+            if not registration:
+                return errors.OFFER_NOT_FOUND
+        else:
+            # Normal registration
+            registration = db.session.query(Registration).filter(
+                Registration.offer_id == offer.id).first()
+
+        # TODO save invitation letter requests, even if emails don't get sent. These can be resend later.
         invitation_letter_request = InvitationLetterRequest(
-            registration_id=registration_id,
+            registration_id=registration.id,
             event_id=event_id,
             work_address=work_address,
             addressed_to=addressed_to,
@@ -53,39 +77,34 @@ class InvitationLetterAPI(InvitationMixin, restful.Resource):
             passport_expiry_date=passport_expiry_date,
             invitation_letter_sent_at=datetime.now().strftime("%Y-%m-%d"),
             to_date=to_date,
-            from_date=from_date,
+            from_date=from_date
         )
 
-        offer = db.session.query(Offer).filter(
-            Offer.user_id == user_id).first()
-
-        if not offer:
-            return errors.OFFER_NOT_FOUND
-
         invitation_template = None
-
-        if offer.accommodation_award and offer.accepted_accommodation_award \
-                and offer.travel_award and offer.accepted_travel_award:
+        # No offer, but a registration = guest registration - TODO check travel and accom assumed for Invited Guests.
+        if(not offer and registration):
+            invitation_template = db.session.query(InvitationTemplate).filter(
+                InvitationTemplate.event_id == event_id).filter(
+                InvitationTemplate.send_for_both_travel_accommodation).first()
+        elif (offer.accommodation_award and offer.accepted_accommodation_award
+                and offer.travel_award and offer.accepted_travel_award):
             invitation_template = db.session.query(InvitationTemplate).filter(
                 InvitationTemplate.event_id == offer.event_id).filter(
                 InvitationTemplate.send_for_both_travel_accommodation).first()
 
-        elif offer.travel_award and offer.accepted_travel_award:
+        elif (offer.travel_award and offer.accepted_travel_award):
             invitation_template = db.session.query(InvitationTemplate).filter(
                 InvitationTemplate.event_id == offer.event_id).filter(
                 InvitationTemplate.send_for_travel_award_only).first()
 
-        elif offer.accommodation_award and offer.accepted_accommodation_award:
+        elif (offer.accommodation_award and offer.accepted_accommodation_award):
             invitation_template = db.session.query(InvitationTemplate).filter(
                 InvitationTemplate.event_id == offer.event_id).filter(
                 InvitationTemplate.send_for_accommodation_award_only).first()
 
-        elif (not offer.accommodation_award) and (not offer.travel_award):
-            invitation_template = db.session.query(InvitationTemplate)\
-                .filter(not InvitationTemplate.send_for_both_travel_accommodation)\
-                .filter(not InvitationTemplate.send_for_travel_award_only)\
-                .filter(not InvitationTemplate.send_for_accommodation_award_only).first()
-
+        elif ((not offer.accommodation_award) and (not offer.travel_award)):
+            invitation_template = db.session.query(InvitationTemplate).filter(
+                not InvitationTemplate.send_for_both_travel_accommodation).filter(not InvitationTemplate.send_for_travel_award_only).filter(not InvitationTemplate.send_for_accommodation_award_only).first()
 
         if invitation_template:
             template_url = invitation_template.template_path
@@ -96,13 +115,16 @@ class InvitationLetterAPI(InvitationMixin, restful.Resource):
             if not user:
                 return errors.USER_NOT_FOUND
 
-            country_of_residence = db.session.query(Country).filter(Country.id == user.residence_country_id).first()
-            nationality = db.session.query(Country).filter(Country.id == user.nationality_country_id).first()
+            country_of_residence = db.session.query(Country).filter(
+                Country.id == user.residence_country_id).first()
+            nationality = db.session.query(Country).filter(
+                Country.id == user.nationality_country_id).first()
             date_of_birth = user.user_dateOfBirth
 
             bringing_poster = ""
 
-            poster_registration_question = db.session.query(RegistrationQuestion).filter(RegistrationQuestion.headline == "Will you be bringing a poster?").first()
+            poster_registration_question = db.session.query(RegistrationQuestion).filter(
+                RegistrationQuestion.headline == "Will you be bringing a poster?").first()
             if poster_registration_question is not None:
                 poster_answer = (
                     db.session.query(RegistrationAnswer)
@@ -116,9 +138,6 @@ class InvitationLetterAPI(InvitationMixin, restful.Resource):
                 if poster_answer is not None and poster_answer.value == "yes":
                     # Get whether they submitted a poster in registration
                     bringing_poster = "The participant will be presenting an academic poster on their research."
-
-                
-
 
             is_sent = generate(template_path=template_url,
                                event_id=event_id,
