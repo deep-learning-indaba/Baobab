ab6d5761031d3a6821e1dd9cab49a04ecc2a1606
Jade Abbott <jabbott@retrorabbit.co.za>
2019-07-04 14:38:32 +0200

    Updated the work_address logic

--- api/app/invitationletter/tests.py
+++ api/app/invitationletter/tests.py
@@ -105,7 +105,7 @@ class InvitationLetterTests(ApiTestCase):
         data = json.loads(response.data)
         header = {'Authorization': data['token']}
         return header
-        
+
     @nottest
     def test_create_create_invitation_letter(self):
         self.seed_static_data()
@@ -122,6 +122,28 @@ class InvitationLetterTests(ApiTestCase):
         assert data['invitation_letter_request_id'] == 1
         assert letter.event_id == 1
         assert letter.work_address == "Somewhere over the rainbow"
+        assert letter.addressed_to == "Sir"
+        assert letter.residential_address == "Way up high"
+        assert letter.passport_name == "Jane Doe"
+        assert letter.passport_no == "23456565"
+        assert letter.passport_issued_by == "Neverland"
+
+    def test_create_create_invitation_letter_no_work(self):
+        self.seed_static_data()
+        INVITATION_LETTER['work_address'] = None
+        response = self.app.post(
+                '/api/v1/invitation-letter', data=INVITATION_LETTER, headers=self.headers)
+        data = json.loads(response.data)
+        print(data)
+        LOGGER.debug("invitation letter: {}".format(data))
+
+        letter = db.session.query(InvitationLetterRequest).filter(
+            InvitationLetterRequest.id == data['invitation_letter_request_id']).first()
+
+        assert response.status_code == 201
+        assert data['invitation_letter_request_id'] == 1
+        assert letter.event_id == 1
+        assert letter.work_address == "Way up high"
         assert letter.addressed_to == "Sir"
         assert letter.residential_address == "Way up high"
         assert letter.passport_name == "Jane Doe"
