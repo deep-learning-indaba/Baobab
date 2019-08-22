14658cee712081ff5e8bd48a2ce9e2db9f28b1cc
Avishkar Bhoopchand <avishkar@google.com>
2019-08-22 13:41:20 +0100

    Updated email text

--- api/app/attendance/emails.py
+++ api/app/attendance/emails.py
@@ -3,7 +3,7 @@ Dear {user_title} {first_name} {last_name},
 
 Welcome to the {event_name}! We're so excited that you made it.
 
-If you have any trouble, please reach out to us on info@deeplearningindaba.com or ask any of our helpful volunteers.
+If you have any questions or issues, please chat to one of the organisers wearing RED t-shirts, post a message on the Slack group or email us at info@deeplearningindaba.com.
 
 Kind Regards,
 The Deep Learning Indaba Team
