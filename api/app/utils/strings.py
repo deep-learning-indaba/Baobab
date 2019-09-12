16f877b542ea6a2b968e5d15d93d547e510211d0
Amrit Purshotam <amrit.purshotam@gmail.com>
2019-09-12 21:02:00 +0200

    Order questions on application confirmation email

--- api/app/utils/strings.py
+++ api/app/utils/strings.py
@@ -10,16 +10,31 @@ Kind Regards,
 The {event_name} Organising Committee
 """
 
+def _get_answer_value(answer):
+    question = answer.question
+    if question.type == 'multi-choice' and question.options is not None:
+        value = [o for o in question.options if o['value'] == answer.value]
+        if not value:
+            return answer.value
+        return value[0]['label']
+    
+    if question.type == 'file' and answer.value:
+        return 'Uploaded File'
+
+    return answer.value
+
 def build_response_email_greeting(title, firstname, lastname):
     return ('Dear {title} {firstname} {lastname},'.format(title=title, firstname=firstname, lastname=lastname))
 
-def build_response_email_body(event_name, event_description, summary):
+def build_response_email_body(event_name, event_description, answers):
     #stringifying the dictionary summary, with linebreaks between question/answer pairs
     stringified_summary = None
-    for key, value in summary.iteritems():
+    for answer in answers:
+        question_headline = answer.question.headline
+        answer_value = _get_answer_value(answer)
         if(stringified_summary is None):
-            stringified_summary = '{question}:\n{answer}'.format(question=key, answer=value)
+            stringified_summary = '{question}:\n{answer}'.format(question=question_headline, answer=answer_value)
         else:
-            stringified_summary = '{current_summary}\n\n{question}:\n{answer}'.format(current_summary=stringified_summary, question=key, answer=value)
+            stringified_summary = '{current_summary}\n\n{question}:\n{answer}'.format(current_summary=stringified_summary, question=question_headline, answer=answer_value)
 
     return (RESPONSE_BODY.format(event_description=event_description, event_name=event_name, summary=stringified_summary))
