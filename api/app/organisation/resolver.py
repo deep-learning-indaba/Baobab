85793a16008df6cfce4b3d5ea02c985f07d2dd1c
Avishkar Bhoopchand <avishkar@google.com>
2020-01-22 17:15:31 +0000

    Fix test... finally?

--- api/app/organisation/resolver.py
+++ api/app/organisation/resolver.py
@@ -7,17 +7,25 @@ class OrganisationResolver():
     _cache = None
 
     @classmethod
+    def _populate_cache(cls):
+        LOGGER.info('Populating Organisation Cache')
+        organisations = OrganisationRepository.get_all()
+        cls._cache = {}
+        for org in organisations:
+            cls._cache[org.domain] = org
+
+    @classmethod
     def resolve_from_domain(cls, domain):
         if not cls._cache:
-            LOGGER.info('Populating Organisation Cache')
-            organisations = OrganisationRepository.get_all()
-            cls._cache = {}
-            for org in organisations:
-                cls._cache[org.domain] = org
+            cls._populate_cache()
         
         if domain not in cls._cache:
-            LOGGER.error('Could not resolve organisation from domain: {}, HTTP Origin: {}, HTTP Referer: {}'.format(
-                domain, request.environ.get('HTTP_ORIGIN', ''), request.environ.get('HTTP_REFERER', '')))
-            raise BadRequest('Could not resolve organisation')
-        else:    
-            return cls._cache[domain]
+            # Re-populate the cache and retry
+            cls._populate_cache()
+            LOGGER.warning('Could not resolve organisation from domain: {}, trying to repopulate cache'.format(domain))
+            if domain not in cls._cache:
+                LOGGER.error('Could not resolve organisation from domain: {}, HTTP Origin: {}, HTTP Referer: {}'.format(
+                    domain, request.environ.get('HTTP_ORIGIN', ''), request.environ.get('HTTP_REFERER', '')))
+                raise BadRequest('Could not resolve organisation')
+        
+        return cls._cache[domain]
