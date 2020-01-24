8c641257970a45eddc8d8962a1e6a37e94af5c83
Jade Abbott <jabbott@retrorabbit.co.za>
2020-01-24 15:57:42 +0200

    no idea

---
+++ api/migrate_database.sh
@@ -0,0 +1,11 @@
+# Install the cloud_sql_proxy
+echo $GCLOUD_SQL_SERVICE_KEY > cloud_proxy.key
+wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O cloud_sql_proxy
+chmod +x cloud_sql_proxy
+sudo mkdir /cloudsql
+sudo chmod 777 /cloudsql
+./cloud_sql_proxy -dir=/cloudsql -instances=baobab:us-central1:baobab -credential_file=cloud_proxy.key & # Run the cloud_sql_proxy
+# Run the migrations
+#cd api
+pip install -r requirements.txt
+DATABASE_URL=$STAGING_DB_PROXY python run.py db upgrade # Temporarily update the database url to the proxy and run migrations
