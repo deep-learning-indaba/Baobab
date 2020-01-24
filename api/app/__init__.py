9c35ad1e09dacb723bde7ec9205b41889ba012a1
Jade Abbott <jabbott@retrorabbit.co.za>
2020-01-24 16:14:50 +0200

    Starting the upgrade to py3

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -6,17 +6,14 @@ from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from flask_redis import FlaskRedis
-from utils.logger import Logger
+from .utils.logger import Logger
 from flask_admin import Admin, AdminIndexView, helpers, expose
 from flask_admin.contrib.sqla import ModelView
 import flask_login as login
 from wtforms import form, fields, validators
 from werkzeug.security import generate_password_hash, check_password_hash
 
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
-
+# Initialize the flask app
 app = Flask(__name__)
 app.config.from_object('config')
 CORS(app, resources={r"/api/*": {"origins": "*"}}, supports_credentials=True)
@@ -27,26 +24,114 @@ bcrypt = Bcrypt(app)
 redis = FlaskRedis(app)
 LOGGER = Logger().get_logger()
 
+# Define the Routes
+from app import rest_api
+from .applicationModel import api as form_api
+from .users import api as users_api
+from .responses import api as responses_api
+from .content import api as content_api
+from .files import api as files_api
+from .events import api as events_api
+from .reviews import api as reviews_api
+from .invitedGuest import api as invitedGuest_api
+from .registration import api as registration_api
+from .registrationResponse import api as registration_response
+from .guestRegistrations import api as guest_registration
+from .invitationletter import api as invitation_letter_api
+from .attendance import api as attendance_api
 
-
-import routes
+rest_api.add_resource(users_api.UserAPI, '/api/v1/user')
+rest_api.add_resource(users_api.UserCommentAPI, '/api/v1/user-comment')
+rest_api.add_resource(users_api.AuthenticationAPI, '/api/v1/authenticate')
+rest_api.add_resource(users_api.PasswordResetRequestAPI,
+                      '/api/v1/password-reset/request')
+rest_api.add_resource(users_api.PasswordResetConfirmAPI,
+                      '/api/v1/password-reset/confirm')
+rest_api.add_resource(users_api.AdminOnlyAPI, '/api/v1/admin')
+rest_api.add_resource(users_api.EmailerAPI,
+                      '/api/v1/admin/emailer')
+rest_api.add_resource(form_api.ApplicationFormAPI, '/api/v1/application-form')
+rest_api.add_resource(responses_api.ResponseAPI, '/api/v1/response')
+rest_api.add_resource(content_api.CountryContentAPI,
+                      '/api/v1/content/countries')
+rest_api.add_resource(files_api.FileUploadAPI, '/api/v1/file')
+rest_api.add_resource(content_api.CategoryContentAPI,
+                      '/api/v1/content/categories')
+rest_api.add_resource(content_api.EthnicityContentAPI,
+                      '/api/v1/content/ethnicity')
+rest_api.add_resource(content_api.TitleContentAPI,
+                      '/api/v1/content/title'),
+rest_api.add_resource(content_api.DisabilityContentAPI,
+                      '/api/v1/content/disability'),
+rest_api.add_resource(content_api.GenderContentAPI,
+                      '/api/v1/content/gender'),
+rest_api.add_resource(events_api.EventsAPI,
+                      '/api/v1/events'),
+rest_api.add_resource(events_api.EventStatsAPI,
+                      '/api/v1/eventstats'),
+rest_api.add_resource(users_api.VerifyEmailAPI,
+                      '/api/v1/verify-email'),
+rest_api.add_resource(users_api.ResendVerificationEmailAPI,
+                      '/api/v1/resend-verification-email'),
+rest_api.add_resource(reviews_api.ReviewAPI, '/api/v1/review')
+rest_api.add_resource(reviews_api.ReviewResponseAPI, '/api/v1/reviewresponse')
+rest_api.add_resource(reviews_api.ReviewAssignmentAPI,
+                      '/api/v1/reviewassignment')
+rest_api.add_resource(reviews_api.ReviewSummaryAPI,
+                      '/api/v1/reviewassignment/summary')
+rest_api.add_resource(events_api.NotSubmittedReminderAPI,
+                      '/api/v1/reminder-unsubmitted')
+rest_api.add_resource(events_api.NotStartedReminderAPI,
+                      '/api/v1/reminder-not-started')
+rest_api.add_resource(reviews_api.ReviewHistoryAPI, '/api/v1/reviewhistory')
+rest_api.add_resource(users_api.UserProfileList, '/api/v1/userprofilelist')
+rest_api.add_resource(users_api.UserProfile, '/api/v1/userprofile')
+rest_api.add_resource(invitedGuest_api.InvitedGuestAPI, '/api/v1/invitedGuest')
+rest_api.add_resource(invitedGuest_api.CreateUser,
+                      '/api/v1/invitedGuest/create')
+rest_api.add_resource(invitedGuest_api.InvitedGuestList,
+                      '/api/v1/invitedGuestList')
+rest_api.add_resource(invitedGuest_api.CheckIfInvitedGuest,
+                      '/api/v1/checkIfInvitedGuest')
+rest_api.add_resource(registration_api.OfferAPI, '/api/v1/offer')
+rest_api.add_resource(registration_api.RegistrationFormAPI,
+                      '/api/v1/registration-form')
+rest_api.add_resource(registration_api.RegistrationSectionAPI,
+                      '/api/v1/registration-section')
+rest_api.add_resource(registration_api.RegistrationQuestionAPI,
+                      '/api/v1/registration-question')
+rest_api.add_resource(registration_response.RegistrationApi,
+                      '/api/v1/registration-response')
+rest_api.add_resource(guest_registration.GuestRegistrationApi,
+                      '/api/v1/guest-registration')
+rest_api.add_resource(guest_registration.GuestRegistrationFormAPI,
+                      '/api/v1/guest-registration-form')
+rest_api.add_resource(registration_response.RegistrationUnconfirmedAPI,
+                      '/api/v1/registration/unconfirmed')
+rest_api.add_resource(registration_response.RegistrationConfirmedAPI,
+                      '/api/v1/registration/confirmed')
+rest_api.add_resource(registration_response.RegistrationConfirmAPI,
+                      '/api/v1/registration/confirm')
+rest_api.add_resource(invitation_letter_api.InvitationLetterAPI,
+                      '/api/v1/invitation-letter')
+rest_api.add_resource(attendance_api.AttendanceAPI, '/api/v1/attendance')
 
 migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
-# set optional bootswatch theme
+# Flask Admin
 app.config['FLASK_ADMIN_SWATCH'] = 'darkly'
-from applicationModel.models import Question, Section
-from responses.models import Response, Answer, ResponseReviewer
-from users.models import UserCategory, AppUser, UserComment
-from events.models import Event, EventRole
+from .applicationModel.models import Question, Section
+from .responses.models import Response, Answer, ResponseReviewer
+from .users.models import UserCategory, AppUser, UserComment
+from .events.models import Event, EventRole
 from app.utils.auth import auth_required, admin_required, generate_token
 from app.utils.errors import UNAUTHORIZED, FORBIDDEN
-from reviews.models import ReviewForm, ReviewQuestion
-from registration.models import Offer, RegistrationForm, RegistrationSection, RegistrationQuestion, Registration, RegistrationAnswer
-from invitationletter.models import InvitationTemplate, InvitationLetterRequest
+from .reviews.models import ReviewForm, ReviewQuestion
+from .registration.models import Offer, RegistrationForm, RegistrationSection, RegistrationQuestion, Registration, RegistrationAnswer
+from .invitationletter.models import InvitationTemplate, InvitationLetterRequest
 # Define login and registration forms (for flask-login)
 class LoginForm(form.Form):
     email = fields.TextField(validators=[validators.required()])
