aca94234bad4fe6854bab0e9b787b1a805d034b3
Amrit Purshotam <amrit.purshotam@gmail.com>
2020-01-25 14:36:10 +0200

    Get email by key and id

--- api/app/email_template/repository.py
+++ api/app/email_template/repository.py
@@ -5,9 +5,5 @@ from app.email_template.models import EmailTemplate
 class EmailRepository():
 
     @staticmethod
-    def get_by_id(email_id):
-        return db.session.query(EmailTemplate).get(email_id)
-
-    @staticmethod
-    def get_by_key(key):
-        return db.session.query(EmailTemplate).filter_by(key=key).first()
+    def get(id, key):
+        return db.session.query(EmailTemplate).filter_by(id=id, key=key).first()
