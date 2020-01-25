f1399e291fc4798fc064ce2081ef17a5904d1be2
Kolawole <bamtak@hotmail.com>
2020-01-25 23:18:30 +0000

    Update event_api to allow querying events by key

--- api/app/events/repository.py
+++ api/app/events/repository.py
@@ -1,5 +1,6 @@
 from app import db
 from app.events.models import Event
+from app.organisation.models import Organisation
 
 
 class EventRepository():
@@ -9,5 +10,21 @@ class EventRepository():
         return db.session.query(Event).get(event_id)
 
     @staticmethod
-    def get_by_key(event_key):
-        return db.session.query(Event).filter_by(key=event_key).first()
+    def exists_by_key(event_key):
+        return db.session.query(Event.id)\
+                         .filter_by(key=event_key)\
+                         .first() is not None
+
+    @staticmethod
+    def get_by_key_with_organisation(event_key):
+        return db.session.query(Event)\
+                         .filter_by(key=event_key)\
+                         .join(Organisation, Organisation.id==Event.organisation_id)\
+                         .first()
+
+    @staticmethod
+    def get_by_id_with_organisation(event_id):
+        return db.session.query(Event)\
+                         .filter_by(id=event_id)\
+                         .join(Organisation, Organisation.id==Event.organisation_id)\
+                         .one_or_none()
