6f47d92bcc9312445bb3407fb3635aa91a860b40
Kolawole <bamtak@hotmail.com>
2020-01-25 14:50:33 +0000

    update event repository with get_by_key

--- api/app/events/repository.py
+++ api/app/events/repository.py
@@ -7,3 +7,7 @@ class EventRepository():
     @staticmethod
     def get_by_id(event_id):
         return db.session.query(Event).get(event_id)
+
+    @staticmethod
+    def get_by_key(event_key):
+        return db.session.query(Event).filter_by(key=event_key).first()
