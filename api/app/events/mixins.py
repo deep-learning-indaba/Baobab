5ae0a8593b6aaf9c8e375be2746a8588d009cf45
s-mawjee <21179950+s-mawjee@users.noreply.github.com>
2020-01-26 17:49:22 +0200

    added event mixin

--- api/app/events/mixins.py
+++ api/app/events/mixins.py
@@ -1,6 +1,15 @@
 from flask_restful import reqparse
 
 
+class EventMixin(object):
+    req_parser = reqparse.RequestParser()
+    req_parser.add_argument('id', type=int, required=False)
+    req_parser.add_argument('name', type=str, required=True)
+    req_parser.add_argument('description', type=str, required=True)
+    req_parser.add_argument('start_date', type=str, required=True)
+    req_parser.add_argument('end_date', type=str, required=True)
+
+
 class EventsMixin(object):
     req_parser = reqparse.RequestParser()
     req_parser.add_argument('event_id', type=int, required=True)
