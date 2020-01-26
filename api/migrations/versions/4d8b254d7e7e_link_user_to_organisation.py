0d6d03b1d85c2406f5d60e1e7eac4ab9b9410196
Avishkar Bhoopchand <avishkar@google.com>
2020-01-26 19:12:54 +0000

    added extend existing option

--- api/migrations/versions/4d8b254d7e7e_link_user_to_organisation.py
+++ api/migrations/versions/4d8b254d7e7e_link_user_to_organisation.py
@@ -22,6 +22,7 @@ Base = declarative_base()
 class Organisation(db.Model):
     
     __tablename__ = "organisation"
+    __table_args__ = {'extend_existing': True}
 
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(50), nullable=False)
