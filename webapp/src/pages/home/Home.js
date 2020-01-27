4b3ef6fbf2f18cf17a61be2d08fa878d7c7be688
NileshArnaiya <nileshjain61@yahoo.in>
2020-01-28 02:19:42 +0530

    cookie consent popup

--- webapp/src/pages/home/Home.js
+++ webapp/src/pages/home/Home.js
@@ -6,6 +6,7 @@ import { applicationFormService } from "../../services/applicationForm";
 import { offerServices } from "../../services/offer/offer.service";
 import { NavLink } from "react-router-dom";
 import { invitedGuestServices } from '../../services/invitedGuests/invitedGuests.service';
+import CookieConsent from "react-cookie-consent";
 
 const DEFAULT_EVENT_ID = process.env.REACT_APP_DEFAULT_EVENT_ID || 1;
 
@@ -21,7 +22,7 @@ class Home extends Component {
       headings: headings,
       rows: [],
       applicationStatus: null,
-      offer:null
+      offer: null
     }
   }
 
@@ -58,7 +59,7 @@ class Home extends Component {
         });
       }
     });
-    
+
     offerServices.getOffer(DEFAULT_EVENT_ID).then(result => {
       this.setState({
         offer: result.offer
@@ -71,13 +72,13 @@ class Home extends Component {
           invitedGuest: true
         });
       }
-      else if (response.statusCode ==="404"){
+      else if (response.statusCode === "404") {
         this.setState({
           invitedGuest: false
         });
       }
     })
-  
+
   }
 
   render() {
@@ -103,7 +104,7 @@ class Home extends Component {
               return (
                 <td className="Cell">
                   {
-                    this.state.rows[rowIndex][fieldNames[cellIndex]] === "Apply now" || 
+                    this.state.rows[rowIndex][fieldNames[cellIndex]] === "Apply now" ||
                       this.state.rows[rowIndex][fieldNames[cellIndex]] === "Continue application" ?
                       <NavLink to="/applicationForm">{this.state.rows[rowIndex][fieldNames[cellIndex]]}</NavLink> :
                       this.state.rows[rowIndex][fieldNames[cellIndex]]
@@ -123,8 +124,8 @@ class Home extends Component {
       ) : (<div></div>)
     }
 
-    let statusClass = this.state.applicationStatus == "Submitted" 
-      ? this.state.offer === null ? "text-warning" : "text-success" 
+    let statusClass = this.state.applicationStatus == "Submitted"
+      ? this.state.offer === null ? "text-warning" : "text-success"
       : "text-danger"
 
     return (
@@ -135,17 +136,17 @@ class Home extends Component {
         {this.props.user && this.state.applicationStatus && <h3 className="Blurb">Application Status for Deep Learning Indaba 2019</h3>}
         {!this.props.user && <h2 className="Blurb">Welcome to Baobab</h2>}
 
-        {(this.props.user && this.state.applicationStatus) && 
+        {(this.props.user && this.state.applicationStatus) &&
           <div>
             <div class="status2019 row text-center">
               <div className="col-sm">
                 <h5 className={statusClass}>
 
                   {this.state.applicationStatus === "Submitted" && this.state.offer && <span>
-                    Your application was successful! 
+                    Your application was successful!
                   </span>}
 
-                  {this.state.applicationStatus === "Submitted" && !this.state.offer &&  this.state.invitedGuest !== true && <span>
+                  {this.state.applicationStatus === "Submitted" && !this.state.offer && this.state.invitedGuest !== true && <span>
                     Waiting List
                   </span>}
 
@@ -156,15 +157,15 @@ class Home extends Component {
                   {this.state.applicationStatus !== "Submitted" && this.state.invitedGuest !== true && this.state.applicationStatus}
                 </h5>
 
-                { this.state.invitedGuest === true && <div>  
-                <p>You've been invited to the Indaba as a guest! Please proceed to registration <NavLink to="/registration">here</NavLink>.</p>
+                {this.state.invitedGuest === true && <div>
+                  <p>You've been invited to the Indaba as a guest! Please proceed to registration <NavLink to="/registration">here</NavLink>.</p>
                 </div>}
 
                 {this.state.applicationStatus == "Submitted" &&
-                <div>
-                  {this.state.offer !== null ? <p>There is an offer waiting for you, <NavLink to="/offer">click here</NavLink> to view it.</p>
-                  : <p>You are currently on the waiting list for the Deep Learning Indaba 2019. Please await further communication.</p>}
-                </div>}
+                  <div>
+                    {this.state.offer !== null ? <p>There is an offer waiting for you, <NavLink to="/offer">click here</NavLink> to view it.</p>
+                      : <p>You are currently on the waiting list for the Deep Learning Indaba 2019. Please await further communication.</p>}
+                  </div>}
 
                 {this.state.applicationStatus == "Withdrawn" && <p>
                   Your application has been withdrawn - you will not be considered for a place at the Indaba.
@@ -174,7 +175,7 @@ class Home extends Component {
                   You did not submit an application to attend the Deep Learning Indaba 2019!
                 </p>}
 
-                {this.state.applicationStatus == "Not Started" && this.state.invitedGuest !== true  && <p>
+                {this.state.applicationStatus == "Not Started" && this.state.invitedGuest !== true && <p>
                   You did not apply to attend the Deep Learning Indaba 2019.
                 </p>}
 
@@ -189,6 +190,18 @@ class Home extends Component {
           //Temporarily removed during campaign to get people to complete their applications
           //this.props.user && table
         }
+        <CookieConsent
+          cookieName="sweetIndabaCookie"
+          style={{ background: "#343a40" }}
+          buttonStyle={{ fontWeight: "bold" }}
+          enableDeclineButton
+          declineButtonText="Decline"
+          buttonText="I accept"
+          buttonClasses="btn btn-primary"
+          containerClasses="alert alert-warning col-lg-12"
+          contentClasses="text-capitalize"
+        > By accepting you agree to our use of cookies and other technologies to process your personal data to give you better functionality and to ensure your experience is consistent between visits.  <a href="/PrivacyPolicy.pdf" style={{color: "white"}}>Learn more >></a>
+        </CookieConsent>
       </div>
     );
   }
