7923a80e3c3806ea3e43f27f2260983117d310cb
Amrit Purshotam <amrit.purshotam@gmail.com>
2020-01-30 18:33:32 +0200

    Add relationship between organisation and event

--- api/app/organisation/models.py
+++ api/app/organisation/models.py
@@ -12,6 +12,8 @@ class Organisation(db.Model):
     domain = db.Column(db.String(100), nullable=False)
     url = db.Column(db.String(100), nullable=False)
 
+    events = db.relationship('Event')
+
     def __init__(self, name, system_name, small_logo, large_logo, domain, url):
         self.name = name
         self.small_logo = small_logo
