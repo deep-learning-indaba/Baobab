b88649ec2f4258f2899b9b88a83ebddd2f78a3f1
Honoré <honormbaya@gmail.com>
2020-02-07 18:42:13 +0200

    Introducing a new API that implements a POST and GET methods, removing POST method from the ancient API

--- api/app/files/api.py
+++ api/app/files/api.py
@@ -6,6 +6,7 @@ from flask import send_file
 from config import GCP_CREDENTIALS_DICT, GCP_PROJECT_NAME, GCP_BUCKET_NAME, FILE_SIZE_LIMIT
 from app.utils.errors import FILE_SIZE_EXCEEDED
 from app.files.mixins import FileUploadMixin
+from app.files.models import File
 from app.utils.auth import auth_required
 
 from google.cloud import storage
@@ -45,9 +46,33 @@ class FileUploadAPI(FileUploadMixin, restful.Resource):
             return send_file(temp.name, as_attachment=True, attachment_filename=args['filename'], mimetype='application/pdf')
 
 
+
+class FileAPIImproved(FileUploadMixin, restful.Resource):
+    def get(self, file_id):
+        # TO DO: Query the File table
+
+        req_parser = reqparse.RequestParser()
+        req_parser.add_argument('filename', type=str, required=True)
+        args = req_parser.parse_args()
+        LOGGER.debug("FileUpload GET args: {}".format(args))
+
+        bucket = _get_storage_bucket()
+
+        blob = bucket.blob(args['filename'])
+
+        # TO DO : Rename the file and set the appropriate mime_type
+
+        with tempfile.NamedTemporaryFile() as temp:
+            blob.download_to_filename(temp.name)
+            return send_file(temp.name, as_attachment=True, attachment_filename=args['filename'], mimetype='application/pdf')
+
+
     @auth_required
     def post(self):
         args = self.req_parser.parse_args()
+        file_name = args['filename']
+        mime_ = args['event_id']
+        email_template = args['email_template']
 
         LOGGER.debug("FileUpload args: {}".format(args))
 
@@ -67,6 +92,9 @@ class FileUploadAPI(FileUploadMixin, restful.Resource):
 
         blob.upload_from_string(bytes_file, content_type=content_type)
 
+        # TO DO : Add a record to File table to store metadata
+
         return {
-            'file_id': unique_name,
+            'file_id': unique_name, # TO DO : Return file id
         }, 201
+
