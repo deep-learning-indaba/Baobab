8249ebeaa42f46ae0e3a0b779ef100d3e3ed8b2d
Kolawole <bamtak@hotmail.com>
2020-02-07 17:10:54 +0000

    Revert "some fix on reference api"
    This reverts commit 2f6dce64f4f7e1f483211232dafcad02d5f81723.

--- api/app/responses/repository.py
+++ api/app/responses/repository.py
@@ -5,5 +5,7 @@ from app.responses.models import Response
 class ResponseRepository():
 
     @staticmethod
-    def get_by_id(response_id):
-        return db.session.query(Response).get(response_id)
+    def get_by_id_and_user_id(response_id, user_id):
+        return db.session.query(Response)\
+                         .filter_by(id=response_id, user_id=user_id)\
+                         .first()
