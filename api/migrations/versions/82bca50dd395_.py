a06685c868b12c54033d10c4f9fead9f4885fe39
Amrit Purshotam <amrit.purshotam@gmail.com>
2020-02-10 17:23:19 +0200

    Merge migrations

---
+++ api/migrations/versions/82bca50dd395_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 82bca50dd395
+Revises: ('3e7bdff8af80', '8a55d9f8cb61')
+Create Date: 2020-02-10 15:21:51.908526
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '82bca50dd395'
+down_revision = ('3e7bdff8af80', '8a55d9f8cb61')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
