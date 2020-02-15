980750cbd9b5b49e1c1219bc3b785ecbc7d4407a
Honoré <honormbaya@gmail.com>
2020-02-15 17:28:24 +0200

    Add a constructor in File model

--- api/app/files/models.py
+++ api/app/files/models.py
@@ -1,3 +1,4 @@
+import uuid
 from app import db
 
 class File(db.Model):
@@ -6,4 +7,9 @@ class File(db.Model):
 	file_name = db.Column(db.String(50), unique=True, nullable=False)
 	mime_type = db.Column(db.String(20), nullable=False)
 	guid = db.Column(db.String(50), unique=True, nullable=False)
+
+	def __init__(self, file_name, mime_type):
+		self.file_name = file_name
+		self.mime_type = mime_type
+		self.guid = str(uuid.uuid4().hex)
 	
