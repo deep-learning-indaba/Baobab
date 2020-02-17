c9adb4688b2aea222b34918b70e93012174dd40b
Honoré <honormbaya@gmail.com>
2020-02-17 23:45:42 +0200

    Dropping some unecessary unique constraints from the model

--- api/app/files/models.py
+++ api/app/files/models.py
@@ -4,9 +4,9 @@ from app import db
 class File(db.Model):
 	__tablename__ = "file"
 	id = db.Column(db.Integer, primary_key=True)
-	file_name = db.Column(db.String(50), unique=True, nullable=False)
-	mime_type = db.Column(db.String(20), nullable=False)
-	guid = db.Column(db.String(50), unique=True, nullable=False)
+	file_name = db.Column(db.String(50), nullable=False)
+	mime_type = db.Column(db.String(50), nullable=False)
+	guid = db.Column(db.String(50), nullable=False)
 
 	def __init__(self, file_name, mime_type):
 		self.file_name = file_name
