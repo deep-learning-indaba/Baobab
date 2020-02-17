2dc288f7c50bc448911079556883ef50b933bf2a
Honoré <honormbaya@gmail.com>
2020-02-17 23:52:20 +0200

    Create a separate repository file

---
+++ api/app/files/repository.py
@@ -0,0 +1,14 @@
+from app import db
+from app.files.models import File 
+
+class FileRepository:
+	@staticmethod
+    def get_by_id(file_id):
+        return db.session.query(File).get(file_id)
+
+
+    @staticmethod
+    def save(file):
+        db.session.add(file)
+        db.session.commit()
+        return file.id
