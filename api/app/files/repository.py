8adeb596420b5146ac8c3ca169e7ed08f6a36929
Honoré <honormbaya@gmail.com>
2020-02-24 22:02:45 +0200

    Fixed some indentation errors, delete a migration and create a merging migration

--- api/app/files/repository.py
+++ api/app/files/repository.py
@@ -3,12 +3,12 @@ from app.files.models import File
 
 class FileRepository:
 	@staticmethod
-    def get_by_id(file_id):
-        return db.session.query(File).get(file_id)
+	def get_by_id(file_id):
+		return db.session.query(File).get(file_id)
 
 
-    @staticmethod
-    def save(file):
-        db.session.add(file)
-        db.session.commit()
-        return file.id
+	@staticmethod
+	def save(file):
+	    db.session.add(file)
+	    db.session.commit()
+	    return file.id
