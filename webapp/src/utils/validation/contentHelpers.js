3a92bcdbecf887e1737cc3681db3d5cd6a4d98f0
Taariq Isaacs <taariq@aerobotics.com>
2020-02-29 16:24:11 +0200

    added gender and country options back

--- webapp/src/utils/validation/contentHelpers.js
+++ webapp/src/utils/validation/contentHelpers.js
@@ -1,2 +1,4 @@
 import { getContent } from "../../services/content";
 export const getTitleOptions = getContent("title");
+export const getGenderOptions = getContent("gender");
+export const getCounties = getContent("countries");
