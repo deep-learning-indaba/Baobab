118d3292294d93a2ad611c52b89ace6aca57f243
Avishkar Bhoopchand <avishkar@google.com>
2020-03-08 21:54:48 +0000

    Changed to use individual checkbox inputs

--- webapp/src/components/form/FormMultiCheckbox.js
+++ webapp/src/components/form/FormMultiCheckbox.js
@@ -1,8 +1,26 @@
 import React from "react";
 import "./Style.css";
-import FormCheckbox from "./FormCheckbox";
+import FormGroup from "./FormGroup";
+import FormToolTip from "./FormToolTip";
 
 class FormMultiCheckbox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      options: []
+    };
+  }
+
+  componentDidMount() {
+    let checkedValues = [];
+    if (this.props.defaultValue) {
+      checkedValues = this.props.defaultValue.split('-');
+    }
+    this.setState({
+      checked: checkedValues
+    });
+  }
+
   shouldDisplayError = () => {
     return this.props.showError && this.props.errorText !== "";
   };
@@ -13,23 +31,62 @@ class FormMultiCheckbox extends React.Component {
     }
   }
 
-  renderFormCheckbox(i) {
+  onCheckChanged = (option, checked) => {
+    this.setState(prevState => {
+      var newChecked = prevState.checked.filter(c => c != option.value);
+      if (checked) {
+        newChecked.push(option.value);
+      }
+      newChecked = newChecked.sort();
+      return {
+        checked: newChecked
+      };
+    })
+  }
+
+  renderFormCheckbox = (option) => {
     return (
-      <FormCheckbox
-        key={i}
-        // checked={this.props.value[i]}
-        onChange={() => this.props.onChange(this.props.options)}
-        label={this.props.options[i]}
-      />
+        <div class="custom-control custom-checkbox">
+          <input
+            id={"checkbox_" + option.value}
+            className={
+              this.shouldDisplayError()
+                ? "custom-control-input is-invalid"
+                : "custom-control-input"
+            }
+            type="checkbox"
+            checked={option.checked}
+            onChange={e => {this.onCheckChanged(option, e.target.value);}}
+            ref={input => {
+              this.nameInput = input;
+            }}
+          />
+          <label class="custom-control-label" for={"checkbox_" + option.value}>{option.label}</label>
+        </div>
     );
   }
 
   render() {
     return (
       <div>
-        {this.props.options.map((text, index) =>
-          {this.renderFormCheckbox(index)}
-        )}
+        <FormGroup
+          id={this.props.Id + "-group"}
+          errorText={this.props.errorText}
+          tabIndex={this.props.tabIndex}
+          autoFocus={this.props.autoFocus}
+        >
+          <div className="rowC">
+            <label htmlFor={this.props.id}>{this.props.label}</label>
+            {this.props.description ? (
+              <FormToolTip description={this.props.description} />
+            ) : (
+              <div />
+            )}
+          </div>
+          <div className="form-inline">
+            {this.props.options.map((option) => this.renderFormCheckbox(option))}
+          </div>
+        </FormGroup>
       </div>
     );
   }
