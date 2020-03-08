b5da8a1176ab16ec11510201fd354d8d55536997
Avishkar Bhoopchand <avishkar@google.com>
2020-03-08 11:53:16 +0000

    Formatting

--- webapp/src/services/references/reference.service.js
+++ webapp/src/services/references/reference.service.js
@@ -8,21 +8,21 @@ export const referenceService = {
     submitReference
 }
 
-function getReferenceRequestDetails(token){
+function getReferenceRequestDetails(token) {
     return axios
-        .get(baseUrl+`/api/v1/reference-request/detail?token=${token}`, {headers: authHeader()})
-        .then((response)=>{
+        .get(baseUrl + `/api/v1/reference-request/detail?token=${token}`, { headers: authHeader() })
+        .then((response) => {
             return {
                 details: response.data,
                 error: ""
             }
         })
-        .catch((error)=>{
+        .catch((error) => {
             return {
                 details: null,
                 error: error.response && error.response.data ?
-                    error.response.data.message:
-                    error.message 
+                    error.response.data.message :
+                    error.message
             }
         });
 }
@@ -31,21 +31,21 @@ function submitReference(token, uploadedDocument, shouldUpdate) {
     let reference = {
         token: token,
         uploaded_document: uploadedDocument
-      };
+    };
 
-    const promise = shouldUpdate 
+    const promise = shouldUpdate
         ? axios.put(baseUrl + "/api/v1/reference", reference, { headers: authHeader() })
         : axios.post(baseUrl + "/api/v1/reference", reference, { headers: authHeader() })
-    
+
     return promise
-        .then(function(response) {
-          return {
-            error: ""
-          };
+        .then(function (response) {
+            return {
+                error: ""
+            };
         })
-        .catch(function(error) {
-          return {
-            error: extractErrorMessage(error)    
-          };
+        .catch(function (error) {
+            return {
+                error: extractErrorMessage(error)
+            };
         });
 }
