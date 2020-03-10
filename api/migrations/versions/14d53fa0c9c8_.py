9adfc47a4840e057440b86ea121de0e0fc0f02bf
Guillaume <guillaume.odendaal@gmail.com>
2020-03-10 15:17:40 +0200

    Added the files for clearing multiple head revisions error

---
+++ api/migrations/versions/14d53fa0c9c8_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 14d53fa0c9c8
+Revises: ('405d106c8292', 'f4bc3108c330')
+Create Date: 2020-03-10 12:29:05.217984
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '14d53fa0c9c8'
+down_revision = ('405d106c8292', 'f4bc3108c330')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
