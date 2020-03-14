afa478dc4c537274ead4766dc7f88a504fa08d62
Amrit Purshotam <amrit.purshotam@gmail.com>
2020-03-14 13:36:14 +0200

    Merge migration

---
+++ api/migrations/versions/f0d16e9a08f8_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: f0d16e9a08f8
+Revises: ('10fa76d53339', 'c3c5ef958ca1')
+Create Date: 2020-03-14 11:34:35.262778
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'f0d16e9a08f8'
+down_revision = ('10fa76d53339', 'c3c5ef958ca1')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
