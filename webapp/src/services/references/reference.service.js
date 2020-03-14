a2cf83b0325d593cc130c588ef1ad28301526a88
Avishkar Bhoopchand <avishkar@google.com>
2020-03-14 23:16:58 +0000

    Added post service method

--- webapp/src/services/references/reference.service.js
+++ webapp/src/services/references/reference.service.js
@@ -33,3 +33,34 @@ function getReferenceRequests(responseId) {
             };
         });
 }
+
+function requestReference(responseId, title, firstname, lastname, email, relation) {
+    data = {
+        response_id: responseId,
+        title: title,
+        firstname: firstname,
+        lastname: lastname,
+        email: email,
+        relation: relation
+    };
+
+    return axios
+        .post(baseUrl + "/api/v1/registration-response", data, {
+            headers: authHeader()
+        })
+        .then(function (response) {
+            return {
+                error: "",
+                referenceRequest: response.data
+            };
+        })
+        .catch(function (error) {
+            return {
+                error:
+                    error.response && error.response.data && error.respose.data.message
+                        ? error.response.data.message
+                        : error.message,
+                referenceRequest: null
+            };
+        });
+}
