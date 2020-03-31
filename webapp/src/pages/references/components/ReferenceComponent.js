acc1f3f6b381508e3859e24b9d830bceb84f8906
Avishkar Bhoopchand <avishkar@google.com>
2020-03-31 23:36:35 +0100

    Tweaks and fixes

--- webapp/src/pages/references/components/ReferenceComponent.js
+++ webapp/src/pages/references/components/ReferenceComponent.js
@@ -4,19 +4,6 @@ import { referenceService } from "../../../services/references";
 import Loading from "../../../components/Loading";
 import FileUploadComponent from "../../../components/FileUpload";
 
-// TODO: Update back-end to format candidate into a single string and populate with AppUser information for self-nominations
-// TODO: Add referer details to ReferenceRequestDetails
-
-const DUMMY_DATA = {
-    candidate: 'Mx Boaty McBoatface',
-    relation: 'Cousin',
-    name: 'Kambule Masters Award',
-    description: 'The Kambule Masters Dissertation Award',
-    is_application_open: true,
-    email_from: 'awards@deeplearningindaba.com',
-    reference_submitted_timestamp: null
-}
-
 class ReferenceComponent extends Component {
     constructor(props) {
         super(props);
@@ -27,7 +14,9 @@ class ReferenceComponent extends Component {
             isLoading: true,
             uploadedDocument: "",
             isSubmitting: false,
-            submitted: false
+            submitted: false,
+            enableSubmit: false,
+            success: false
         }
     }
 
@@ -35,10 +24,11 @@ class ReferenceComponent extends Component {
         referenceService.getReferenceRequestDetails(this.props.match.params.token)
             .then(response => {
                 this.setState({
-                    requestDetails: DUMMY_DATA, // response.details,
-                    error: "", // response.error,
-                    isLoading: false
-                })
+                    requestDetails: response.details,
+                    error: response.error,
+                    isLoading: false,
+                    submitted: response.details && !!response.details.reference_submitted_timestamp
+                });
             });
     }
 
@@ -56,25 +46,27 @@ class ReferenceComponent extends Component {
 
     onFileUploadChanged = (_, value) => {
         this.setState({
-            uploadedDocument: value
+            uploadedDocument: value,
+            enableSubmit: true
         });
     }
 
     submit = (event) => {
         this.setState({ isSubmitting: true });
-        referenceService.submitReference(this.state.token, this.state.uploadedDocument).then(
+        referenceService.submitReference(this.props.match.params.token, this.state.uploadedDocument, this.state.submitted).then(
             response => {
                 this.setState({
                     error: response.error,
                     isSubmitting: false,
                     submitted: true,
-                    uploadedDocument: ""
-                })
+                    enableSubmit: false,
+                    success: true
+                });
             });
     }
 
     render() {
-        const { requestDetails, error, isLoading, uploadedDocument, isSubmitting } = this.state;
+        const { requestDetails, error, isLoading, uploadedDocument, isSubmitting, enableSubmit, success } = this.state;
         if (isLoading) { return <Loading />; }
 
         if (error) {
@@ -93,18 +85,21 @@ class ReferenceComponent extends Component {
             );
         }
 
+        const nominationText = requestDetails.nominator
+            ? `has been nominated by ${requestDetails.nominator}`
+            : "has nominated themself"
+
         return (
             <div>
                 <h1>Reference</h1>
 
                 {requestDetails.reference_submitted_timestamp
-                    && <div className="card flat-card success stretched">
+                    && <div className="alert alert-success alert-container">
                         Thank you! You submitted your reference on {requestDetails.reference_submitted_timestamp}. You may update it below if you like.
                     </div>
                 }
 
-                {/* TODO: Add details about nominator and nominee when it's not a self-nomination */}
-                <p>{requestDetails.candidate} has requested your reference in support of their application to the {requestDetails.description}. Kindly upload your reference in the form of a PDF (max 10Mb) below.</p>
+                <p>{requestDetails.candidate} {nominationText} for the {requestDetails.description}. Kindly upload your reference in support of their application in the form of a PDF (max 10Mb) below.</p>
                 <div className="card stretched">
                     <h5>Upload Reference (PDF, 10Mb max)</h5>
                     <FileUploadComponent
@@ -114,11 +109,17 @@ class ReferenceComponent extends Component {
                         key="fileupload" />
                 </div>
 
+                {success &&
+                    <div className={"alert alert-success alert-container"}>
+                        Thank you! Your reference has been received and will be reviewed by our selection committee.
+                    </div>
+                }
+
                 <button
                     type="submit"
                     class="btn btn-primary margin-top-32"
                     onClick={this.submit}
-                    disabled={!uploadedDocument}
+                    disabled={!enableSubmit}
                 >
                     {isSubmitting && (
                         <span
