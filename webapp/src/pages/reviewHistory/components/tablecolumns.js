cdbceb854819e20b954811f5ae4cc6f332586a14
Avishkar Bhoopchand <avishkar@google.com>
2020-04-14 22:03:32 +0100

    Update review history

--- webapp/src/pages/reviewHistory/components/tablecolumns.js
+++ webapp/src/pages/reviewHistory/components/tablecolumns.js
@@ -6,35 +6,14 @@ export const columns = [
     Header: " ",
     accessor: "review_response_id",
     Cell: row => {
-      return <Link to={"/review/" + row.row.review_response_id}>
+      return <Link to={"review/" + row.row.review_response_id}>
         <i className="fa fa-edit"></i></Link>
     },
     filterable: false
-  }, {
+  }, 
+  {
     Header: "Submitted Timestamp",
     accessor: "submitted_timestamp",
     filterable: false
-  }, {
-    Header: "Nationality",
-    accessor: "nationality_country",
-    filterable: false
-  }, {
-    Header: "Country of Residence",
-    accessor: "residence_country",
-    filterable: false
-  }, {
-    Header: "Affiliation",
-    accessor: "affiliation",
-    filterable: false
-  }, {
-    Header: "Department",
-    accessor: "department",
-    filterable: false
-  }, {
-    Header: "User Category",
-    accessor: "user_category"
-  }, {
-    Header: "Final Verdict",
-    accessor: "final_verdict"
   }
 ];
