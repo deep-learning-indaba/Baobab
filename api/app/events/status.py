b9348ac4742dacd1a7cd04626c02d33d831c5b20
Avishkar Bhoopchand <avi@avi-net.co.za>
2020-04-29 23:18:06 +0100

    Added event status business logic

---
+++ api/app/events/status.py
@@ -0,0 +1,70 @@
+from app.events.models import Event
+from app.users.models import AppUser
+from app.invitedGuest.repository import InvitedGuestRepository as invited_guest_repository
+from app.responses.repository import ResponseRepository as response_repository
+from app.registrationResponse.repository import RegistrationRepository as registration_repository
+from app.registration.repository import OfferRepository as offer_repository
+from app.outcome.repository import OutcomeRepository as outcome_repository
+
+
+class EventStatus():
+    """Represents the status of a user at an event."""
+    def __init__(self, 
+                 invited_guest=None, 
+                 application_status=None,
+                 outcome_status=None,
+                 offer_status=None, 
+                 registration_status=None):
+        if invited_guest is None and registration_status is not None and offer_status is None:
+            raise ValueError('Non-invited guest must have an offer to have a registration status')
+        
+        self.invited_guest=invited_guest 
+        self.application_status = application_status
+        self.registration_status=registration_status
+        self.offer_status=offer_status
+        self.outcome_status=outcome_status
+
+
+def get_event_status(event, user):
+    invited_guest = invited_guest_repository.get_for_event_and_user(event.id, user.id)
+    if invited_guest:
+        registration = invited_guest_repository.get_registration_for_event_and_user(event.id, user.id)
+        # If they're an invited guest, we don't bother with whether they applied or not
+        return EventStatus(invited_guest=invited_guest.role, 
+                           registration_status=True if registration is not None else False)
+    
+    response = response_repository.get_by_user_id_for_event(user.id, event.id)
+    if response is None:
+        application_status = 'Not Started'
+    elif response.is_submitted:
+        application_status = 'Submitted'
+    elif response.is_withdrawn:
+        application_status = 'Withdrawn'
+    else:
+        application_status = 'Not Submitted'
+
+    outcome = outcome_repository.get_latest_by_user_for_event(user.id, event.id)
+    if outcome is None:
+        outcome_status = None
+    else:
+        outcome_status = outcome.status.name
+
+    offer = offer_repository.get_by_user_id_for_event(user.id, event.id)
+    if offer is None:
+        offer_status = None
+    elif offer.candidate_response:
+        offer_status = 'Accepted'
+    elif offer.candidate_response == False:
+        offer_status = 'Rejected'
+    elif offer.is_expired():
+        offer_status = 'Expired'
+    else:
+        offer_status = 'Pending'
+
+    registration = registration_repository.get_by_user_id(user.id, event.id)
+    registration_status = registration is not None
+
+    return EventStatus(application_status=application_status,
+                       outcome_status=outcome_status,
+                       offer_status=offer_status,
+                       registration_status=registration_status)
