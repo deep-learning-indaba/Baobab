d23dd48fe60657a063e4dbb7b245546409498e2c
Avishkar Bhoopchand <avi@avi-net.co.za>
2020-04-29 23:17:55 +0100

    Added repositories

---
+++ api/app/invitedGuest/repository.py
@@ -0,0 +1,20 @@
+from app import db
+from app.invitedGuest.models import InvitedGuest, GuestRegistration
+from app.registration.models import RegistrationForm
+
+class InvitedGuestRepository():
+
+    @staticmethod
+    def get_for_event_and_user(event_id, user_id):
+        return (db.session.query(InvitedGuest)
+                .filter(InvitedGuest.event_id == event_id, InvitedGuest.user_id == user_id)
+                .first())
+
+    @staticmethod
+    def get_registration_for_event_and_user(event_id, user_id):
+        return (db.session.query(GuestRegistration)
+                .filter(GuestRegistration.user_id == user_id)
+                .join(RegistrationForm, GuestRegistration.registration_form_id == RegistrationForm.id)
+                .filter(RegistrationForm.event_id == event_id)
+                .first())
+
