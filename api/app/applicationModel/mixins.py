170cedffa194e0bf2c7d0c957a3a8f134441f3e8
siobhan <43542098+SMHall94@users.noreply.github.com>
2020-07-06 08:31:25 +0200

    feat(app-mixins): updated req_parser for app form, sections and questions

--- api/app/applicationModel/mixins.py
+++ api/app/applicationModel/mixins.py
@@ -5,4 +5,7 @@ class ApplicationFormMixin(object):
 
     req_parser = reqparse.RequestParser()
     req_parser.add_argument('event_id', type=int, required=True, help = 'Invalid event_id requested. Event_id\'s should be of type int.')
-    
+    req_parser.add_argument('is_open', type=bool, required=True)
+    req_parser.add_argument('nominations', type=bool, required=True)
+    req_parser.add_argument('sections', type=dict, required=True, action='append')
+    req_parser.add_argument('questions', type=dict, required=True, action='append')
