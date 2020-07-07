e6f449adc8081cf12d599368122fdcd57d6517e9
siobhan <43542098+SMHall94@users.noreply.github.com>
2020-07-07 07:51:08 +0200

    Initial planning commited

--- api/app/applicationModel/tests.py
+++ api/app/applicationModel/tests.py
@@ -24,28 +24,28 @@ class ApplicationFormApiTest(ApiTestCase):
         self.test_event = self.add_event('Test Event', 'Event Description', self.start_time, self.end_time)
         self.add_to_db(self.test_event)
         self.test_form = self.create_application_form(
-                self.test_event.id, True, False)
+            self.test_event.id, True, False)
         self.add_to_db(self.test_form)
         self.test_section = Section(
-                self.test_form.id, 'Test Section', 'Test Description', 1)
+            self.test_form.id, 'Test Section', 'Test Description', 1)
         self.add_to_db(self.test_section)
         self.test_question = Question(
-                application_form_id=self.test_form.id, section_id=self.test_section.id,
-                headline='Test Question Headline', placeholder='Test question placeholder',
-                order=1, questionType='multi-choice',
-                validation_regex=None, is_required=True,
-                description='Test Question Description', options=None)
+            application_form_id=self.test_form.id, section_id=self.test_section.id,
+            headline='Test Question Headline', placeholder='Test question placeholder',
+            order=1, questionType='multi-choice',
+            validation_regex=None, is_required=True,
+            description='Test Question Description', options=None)
         self.add_to_db(self.test_question)
         # Section 2
         self.test_section2 = Section(
-                self.test_form.id, 'Test Section 2', 'Test Description 2', 2)
+            self.test_form.id, 'Test Section 2', 'Test Description 2', 2)
         self.add_to_db(self.test_section2)
         self.test_question2 = Question(
-                application_form_id=self.test_form.id, section_id=self.test_section2.id,
-                headline='Test Question 2 Headline', placeholder='Test question 2 placeholder',
-                order=1, questionType='text',
-                validation_regex=None, is_required=True,
-                description='Test Question 2 Description')
+            application_form_id=self.test_form.id, section_id=self.test_section2.id,
+            headline='Test Question 2 Headline', placeholder='Test question 2 placeholder',
+            order=1, questionType='text',
+            validation_regex=None, is_required=True,
+            description='Test Question 2 Description')
         self.add_to_db(self.test_question2)
 
     def test_get_form(self):
@@ -69,10 +69,10 @@ class ApplicationFormApiTest(ApiTestCase):
         sections_by_app_id = application_form_repository.get_sections_by_app_id(self.test_form.id)
         self.assertEqual(sections_by_app_id[0], self.test_section)
         self.assertEqual(sections_by_app_id[1], self.test_section2)
-        sections_by_app_id_and_section_name = application_form_repository.\
+        sections_by_app_id_and_section_name = application_form_repository. \
             get_section_by_app_id_and_section_name(self.test_form.id, self.test_section.name)
         self.assertEqual(sections_by_app_id_and_section_name, self.test_section)
-        sections_by_app_id_and_section_name = application_form_repository.\
+        sections_by_app_id_and_section_name = application_form_repository. \
             get_section_by_app_id_and_section_name(self.test_form.id, self.test_section2.name)
         self.assertEqual(sections_by_app_id_and_section_name, self.test_section2)
 
@@ -85,3 +85,55 @@ class ApplicationFormApiTest(ApiTestCase):
 
         question2 = application_form_repository.get_question_by_id(self.test_question2.id)
         self.assertEqual(question2, self.test_question2)
+
+
+class ApplicationFormCreateTest(ApiTestCase):
+    """
+    Test that an application form is created by an event admin
+    """
+    def _seed_data_create(self):
+        """
+        Mock data used for test simulation
+        """
+        pass
+
+    def test_event_admin_permission(self):
+        """
+        Tests that the application form is being created by a user with the correct permissions
+        """
+        pass
+
+    def test_app_form_is_created(self):
+        """
+        Tests that the application form is created and an ID is returned
+        """
+        pass
+
+    def test_sections_are_created(self):
+        """
+        Tests that the correct sections are added and can be accessed by the respective app id
+        """
+        pass
+
+
+class ApplicationFormUpdateTest(ApiTestCase):
+    """
+    Test that an application form's sections and question are updated
+    """
+    def _seed_data_update(self):
+        """
+        Mock data used for test simulation
+        """
+        pass
+
+    def test_section_id_matches_app_id(self):
+        """
+        Confirm the sections to be updated belong to the respective application form
+        """
+        pass
+
+    def test_updated_app_form_returned(self):
+        """
+        Tests that the new, updated application form is returned
+        """
+        pass
