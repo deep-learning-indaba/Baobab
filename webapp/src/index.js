254a9d9a2c2f3d5798c9efc7643d79d5fecff99f
Jean <greeffjean@gmail.com>
2020-07-21 17:26:41 +0200

    Error message working for ConfirmPasswordResetForm

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -2,7 +2,6 @@ import "react-app-polyfill/ie9";
 import "array-flat-polyfill";
 import React from "react";
 import ReactDOM from "react-dom";
-import Router from 'react-router-dom';
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
@@ -11,12 +10,10 @@ import { ErrorBoundary } from "react-error-boundary";
 require("dotenv").config();
 
 ReactDOM.render(
-  <Router>
   <ErrorBoundary FallbackComponent={ErrorPage}>
     <App />
   </ErrorBoundary>
-  </Router>,
-  document.getElementById("root")
+  , document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
