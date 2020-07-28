c56591d901910b323edbe6b50a918568bca7c436
Jean <greeffjean@gmail.com>
2020-07-28 13:15:55 +0200

    test

--- webapp/src/components/form/_tests_/FormDate.test.js
+++ webapp/src/components/form/_tests_/FormDate.test.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import AppicationForm from '../../../pages/applicationForm/components/ApplicationForm'
+import { shallow, mount } from 'enzyme';
+import AppicationForm from '../../../pages/applicationForm/components/ApplicationForm';
+import FormDate from '../FormDate'
 
 describe('FormDate Componenet Tests', () => {
-    it('Check if FormDate receives and displays error', () => {
+    it('Check if FormDate renders', () => {
+        const wrapper = shallow(<FormDate />);
+        expect(wrapper.length).toEqual(1);
+    })
 
+    it('Check if FormDate renders', () => {
         const wrapper = shallow(<AppicationForm />);
         expect(wrapper.length).toEqual(1);
-
     })
+
+    /*
+     it('loads profile', () => { 
+        const wrapper = mount(<AppicationForm />);
+        wrapper.setProps({ person: "something_different" });
+        expect(wrapper.props().person).toEqual("something_different");
+    })
+    */
+
+   
 
 });
