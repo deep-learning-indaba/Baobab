4ed4f19c049f9864e58c3dbea5a792a79e2db6b9
Jean <greeffjean@gmail.com>
2020-07-30 20:03:28 +0200

    Rearernged setup for MarkDownForm for to make testing easier

---
+++ webapp/src/components/form/MarkDownForm.js
@@ -0,0 +1,59 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import MarkdownIt from 'markdown-it'
+import MdEditor from 'react-markdown-editor-lite'
+
+import 'react-markdown-editor-lite/lib/index.css';
+
+
+const mdParser = new MarkdownIt();
+
+class MarkDownEditor extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            inputValue: ""
+        }
+    }
+    
+
+ handleEditorChange({html, text}) {    
+  this.setState({
+    inputValue: text
+  })
+
+  this.props.onChange({html, text})
+}
+
+
+ onImageUpload(file) {
+    return new Promise(resolve => {
+      const reader = new FileReader();
+      reader.onload = data => {
+          console.log(data.target.result.text)
+        this.props.onImageUpload(file)
+        resolve(data.target.result);
+      };
+      reader.readAsDataURL(file);
+    });
+  }
+
+
+ render () {
+    return (
+        <MdEditor
+          value={this.state.inputValue}
+          style={{ height: "200px" }}
+          renderHTML={(text) => mdParser.render(text)}
+          onChange={(e) => this.handleEditorChange(e)}
+          onImageUpload={(e) => this.onImageUpload(e)}
+          config={{
+            imageAccept: ['.png', '.jpg', '.gif']
+          }}
+          />
+      )
+ }
+
+}
+
+export default MarkDownEditor
