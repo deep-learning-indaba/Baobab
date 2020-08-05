0a1d02c722442d6e16ec6d35b6847d8acad5a33c
Kaleab <kaleabtessera@gmail.com>
2020-08-05 22:06:36 +0200

    Mock out react-i18next in jest.

--- webapp/src/pages/resetPassword/__tests__/ResetPassword.test.js
+++ webapp/src/pages/resetPassword/__tests__/ResetPassword.test.js
@@ -4,6 +4,14 @@ import ResetPassword from '../ResetPassword.js';
 import ConfirmPasswordResetForm from '../components/ConfirmPasswordResetForm';
 
 
+jest.mock('react-i18next', () => ({
+  // this mock makes sure any components using the translate HoC receive the t function as a prop
+  withTranslation: () => Component => {
+    Component.defaultProps = { ...Component.defaultProps, t: () => "" };
+    return Component;
+  },
+}));
+
 describe('ResetPassword render', () => {
   it("ResetPassword render", () => {
     // Render ResetPassword main component.
