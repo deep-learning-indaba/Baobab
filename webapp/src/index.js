ccbda7d810567cf231cd093423cace278be37356
Avishkar Bhoopchand <avi@avi-net.co.za>
2020-08-12 23:34:17 +0100

    Initialising i18next after loading organisation

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,18 +1,60 @@
 import "react-app-polyfill/ie9";
 import "array-flat-polyfill";
-import React from "react";
+import React, { Component, Suspense } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { ErrorPage } from "./components/ErrorPage";
 import { ErrorBoundary } from "react-error-boundary";
-import './i18n';
+import i18nInit from './i18n';
+import Loading from "./components/Loading";
+import { organisationService } from "./services/organisation/organisation.service";
+
+const App = React.lazy(() => import('./App')); // Lazy-loaded
+
 require("dotenv").config();
+
+class Bootstrap extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      organisation: null,
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    organisationService.getOrganisation().then(response => {
+      this.setState({
+        organisation: response.organisation,
+        error: response.error,
+        loading: false
+      });
+      if (response.organisation) {
+        document.title =
+          response.organisation.system_name +
+          " | " +
+          response.organisation.name;
+        i18nInit(response.organisation);
+      }
+    });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <Loading/>
+    }
+
+    return <Suspense fallback={<Loading />}>
+      <App organisation={this.state.organisation}/>
+    </Suspense>
+  }
+}
 
 ReactDOM.render(
   <ErrorBoundary FallbackComponent={ErrorPage}>
-    <App />
+    <Bootstrap />
   </ErrorBoundary>
   , document.getElementById("root")
 );
