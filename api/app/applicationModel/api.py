76a2ff6aefbaebce79b6310df0cc498552fa4401
Amrit Purshotam <amrit.purshotam@gmail.com>
2020-08-14 19:42:43 +0200

    Clean messy logic with SQL Alchemy relationships

--- api/app/applicationModel/api.py
+++ api/app/applicationModel/api.py
@@ -7,7 +7,8 @@ from sqlalchemy.exc import SQLAlchemyError
 
 from app.applicationModel.mixins import ApplicationFormMixin
 from app.applicationModel.models import ApplicationForm, Question, Section
-
+from app.applicationModel.repository import ApplicationFormRepository as application_form_repository
+from app.utils.auth import auth_required
 from app.utils.errors import EVENT_NOT_FOUND, QUESTION_NOT_FOUND, SECTION_NOT_FOUND, DB_NOT_AVAILABLE, FORM_NOT_FOUND, APPLICATIONS_CLOSED
 
 from app import db, bcrypt
@@ -18,75 +19,55 @@ class ApplicationFormAPI(ApplicationFormMixin, restful.Resource):
     question_fields = {
         'id': fields.Integer,
         'type': fields.String,
-        'description': fields.String,
-        'headline': fields.String,
+        # 'description': fields.String,
+        # 'headline': fields.String,
         'order': fields.Integer,
-        'options': fields.Raw,
-        'placeholder': fields.String,
-        'validation_regex': fields.String,
-        'validation_text': fields.String,
+        # 'options': fields.Raw,
+        # 'placeholder': fields.String,
+        # 'validation_regex': fields.String,
+        # 'validation_text': fields.String,
         'is_required': fields.Boolean,
         'depends_on_question_id': fields.Integer,
-        'show_for_values': fields.Raw,
+        # 'show_for_values': fields.Raw,
         'key': fields.String
     }
 
     section_fields = {
         'id': fields.Integer,
-        'name': fields.String,
-        'description': fields.String,
+        #'name': fields.String,
+        #'description': fields.String,
         'order': fields.Integer,
-        'questions': fields.List(fields.Nested(question_fields)),
         'depends_on_question_id': fields.Integer,
-        'show_for_values': fields.Raw
+        'show_for_values': fields.Raw,
+        'questions': fields.List(fields.Nested(question_fields)),
     }
 
     form_fields = {
         'id': fields.Integer,
         'event_id': fields.Integer,
         'is_open':  fields.Boolean,
-        'deadline': fields.DateTime,
-        'sections': fields.List(fields.Nested(section_fields)),
-        'nominations': fields.Boolean  
+        'nominations': fields.Boolean,
+        'sections': fields.List(fields.Nested(section_fields))
     }
 
+    @auth_required
+    @marshal_with(form_fields)
     def get(self):
-        LOGGER.debug('Received get request for application form')
         args = self.req_parser.parse_args()
-        LOGGER.debug('Parsed Args for event_id: {}'.format(args))
+        language = args['language']
 
         try:
-            form = db.session.query(ApplicationForm).filter(ApplicationForm.event_id == args['event_id']).first()     
-            if(not form):
-                LOGGER.warn('Form not found for event_id: {}'.format(args['event_id']))
+            form = application_form_repository.get_by_event_id(args['event_id'])
+            if not form:
                 return FORM_NOT_FOUND
 
             if not form.is_open:
                 return APPLICATIONS_CLOSED
 
-            sections = db.session.query(Section).filter(Section.application_form_id == form.id).all()   #All sections in our form
-            if(not sections):
-                LOGGER.warn('Sections not found for event_id: {}'.format(args['event_id']))
+            if not form.sections:
                 return SECTION_NOT_FOUND
 
-            questions = db.session.query(Question).filter(Question.application_form_id == form.id).all() #All questions in our form        
-            if(not questions):
-                LOGGER.warn('Questions not found for  event_id: {}'.format(args['event_id']))
-                return QUESTION_NOT_FOUND
-
-            form.sections = sections
-
-            for s in form.sections:
-                s.questions = []
-                for q in questions:
-                    if(q.section_id == s.id):
-                        s.questions.append(q)
-
-            if (form): 
-                return marshal(form, self.form_fields)
-            else:
-                LOGGER.warn("Event not found for event_id: {}".format(args['event_id'])) 
-                return EVENT_NOT_FOUND
+            return form
 
         except SQLAlchemyError as e:
             LOGGER.error("Database error encountered: {}".format(e))
--- api/app/applicationModel/models.py
+++ api/app/applicationModel/models.py
@@ -8,14 +8,43 @@ class ApplicationForm(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     event_id = db.Column(db.Integer(), db.ForeignKey('event.id'), nullable=False)
     is_open = db.Column(db.Boolean(), nullable=False)
+    nominations = db.Column(db.Boolean(), nullable=False)
 
     event = db.relationship('Event', foreign_keys=[event_id])
-    nominations = db.Column(db.Boolean(), nullable=False)
+    sections = db.relationship('Section', order_by='Section.order')
 
     def __init__(self, event_id, is_open, nominations):
         self.event_id = event_id
         self.is_open = is_open
         self.nominations = nominations
+
+class Question(db.Model):
+    __tablename__ = 'question'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    application_form_id = db.Column(db.Integer(), db.ForeignKey('application_form.id'), nullable=False)
+    section_id = db.Column(db.Integer(), db.ForeignKey('section.id'), nullable=False)
+    type = db.Column(db.String(), nullable=False)
+    order = db.Column(db.Integer(), nullable=False)
+    is_required = db.Column(db.Boolean(), nullable=False)
+    depends_on_question_id = db.Column(db.Integer(), db.ForeignKey('question.id'), nullable=True)
+    key = db.Column(db.String(255), nullable=True)
+
+    section = db.relationship('Section', foreign_keys=[section_id])
+    question_translations = db.relationship('QuestionTranslation', lazy='dynamic')
+
+    def __init__(self, application_form_id, section_id, headline, placeholder, order, questionType, validation_regex, validation_text=None, is_required = True, description = None, options = None):
+        self.application_form_id = application_form_id
+        self.section_id = section_id
+        self.headline = headline
+        self.placeholder = placeholder
+        self.order = order
+        self.type = questionType
+        self.description = description
+        self.options = options
+        self.is_required = is_required
+        self.validation_regex = validation_regex
+        self.validation_text = validation_text
 
 
 class Section(db.Model):
@@ -28,7 +57,9 @@ class Section(db.Model):
     show_for_values = db.Column(db.JSON(), nullable=True)
     key = db.Column(db.String(255), nullable=True)
 
+    application_form = db.relationship('ApplicationForm', foreign_keys=[application_form_id])
     section_translations = db.relationship('SectionTranslation', lazy='dynamic')
+    questions = db.relationship('Question', primaryjoin=id==Question.section_id, order_by='Question.order')
 
     def __init__(self, application_form_id, name, description, order):
         self.application_form_id = application_form_id
@@ -53,34 +84,6 @@ class SectionTranslation(db.Model):
         self.name = name
         self.description = description
         self.language = language
-
-
-class Question(db.Model):
-    __tablename__ = 'question'
-
-    id = db.Column(db.Integer(), primary_key=True)
-    application_form_id = db.Column(db.Integer(), db.ForeignKey('application_form.id'), nullable=False)
-    section_id = db.Column(db.Integer(), db.ForeignKey('section.id'), nullable=False)
-    type = db.Column(db.String(), nullable=False)
-    order = db.Column(db.Integer(), nullable=False)
-    is_required = db.Column(db.Boolean(), nullable=False)
-    depends_on_question_id = db.Column(db.Integer(), db.ForeignKey('question.id'), nullable=True)
-    key = db.Column(db.String(255), nullable=True)
-
-    question_translations = db.relationship('QuestionTranslation', lazy='dynamic')
-
-    def __init__(self, application_form_id, section_id, headline, placeholder, order, questionType, validation_regex, validation_text=None, is_required = True, description = None, options = None):
-        self.application_form_id = application_form_id
-        self.section_id = section_id
-        self.headline = headline
-        self.placeholder = placeholder
-        self.order = order
-        self.type = questionType
-        self.description = description
-        self.options = options
-        self.is_required = is_required
-        self.validation_regex = validation_regex
-        self.validation_text = validation_text
 
 
 class QuestionTranslation(db.Model):
