7e15f4e47cda9fd4a7c90e055824c396ec7a2351
Jean <greeffjean@gmail.com>
2020-08-17 15:49:20 +0200

    Adding more functionality to FormMultiFile and Child

--- webapp/src/components/form/FormMultiFile.js
+++ webapp/src/components/form/FormMultiFile.js
@@ -8,43 +8,131 @@ class FormMultiFile extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            uploads: [1],
-            fileList: []
+            uploads: [{ file: 1, deleted: false }],
+            fileList: [],
+            addError: false
         }
     }
 
-    addFile() {
-        this.setState({
-            uploads: this.state.uploads.concat(1)
+    // add file
+    addFile = () => {
+        let condition = true;
+        let addError = false;
+        const uploads = this.state.uploads;
+        const fileList = this.state.fileList;
+        let filterDelItems = uploads.filter((val, index) => {
+            return val.deleted != true
         })
+        console.log(filterDelItems)
+        // Check uplaods array for le
 
-    }
+        if (filterDelItems.length > fileList.length) {
+            condition = false;
+            addError = true
+        }
+        else {
+            addError = false
+            condition = true;
+        }
 
-    handleUpload(file, description) {
-
-        var newFile = { name: `${file}`, file: file, description: description }
 
         this.setState({
-            fileList: this.state.fileList.concat(newFile)
-        }, () => this.props.uploadFile(file))
+            uploads: condition ? uploads.concat({ file: uploads.length + 1, deleted: false }) : uploads,
+            addError: addError
+        }, () => console.log(this.state.addError))
+
 
     }
+
+    //handle upload
+    handleUpload = (file, name, del) => {
+
+        // function variables
+        let handleDuplicates = false;
+        let handleList = this.state.fileList;
+        let uploads = this.state.uploads;
+        let removeFile;
+
+
+        // assign data
+        var newFile = { name: `${name}`, file: file }
+
+        // handle Delete
+        if (del) {
+            if (uploads.length == 1) {
+                uploads.concat({ file: null, deleted: true })
+            }
+            else {
+                handleList.map(val => {
+                    if (val.file == file) {
+                        removeFile = handleList.indexOf(val)
+                        handleList.splice(handleList.indexOf(val), 1)
+                        console.log(uploads)
+                        console.log(removeFile)
+
+                    }
+                })
+
+                if (removeFile > -1) {
+                    uploads[removeFile] = { file: null, deleted: true }
+                }
+            }
+
+        }
+
+        // if not delete then handle otherwise
+        else {
+            // test and handle updated values
+            handleList.map(val => {
+                if (val.file == file) {
+                    handleList.splice(handleList.indexOf(val), 1, newFile)
+                    handleDuplicates = true;
+                }
+            })
+
+            // concat or return if file is just being updated
+            if (!handleDuplicates) {
+                handleList.push(newFile)
+                this.props.uploadFile(file)
+            }
+        }
+
+        // setState and Callback functions
+        this.setState({
+            fileList: handleList,
+            uploads: uploads,
+
+        },  // reset function variables
+            () => {
+                handleDuplicates = false;
+                removeFile = null;
+            })
+
+        console.log(this.state.fileList)
+        console.log(this.state.uploads)
+    }
+
 
     render() {
         return (
             <div>
                 <FormGroup>
                     {this.state.uploads.map((val) => {
-                        return <MultiFileComponent className="multi-file-component"
-                            handleUpload={(file, description, paramType) => this.handleUpload(file, description)}
-                        />
+                        if (!val.deleted)
+                            return <MultiFileComponent className="multi-file-component"
+                                handleUpload={(file, name, del) => this.handleUpload(file, name, del)}
+                                errorText={this.props.errorText}
+                                addError={this.state.addError}
+                                del={(file) => this.del(file)}
+                            />
                     })}
 
-                    <button className="add-file-btn" onClick={(e) => this.addFile(e)}>+</button>
+                    <button className="add-file-btn" onClick={(e) => this.addFile(e)}>Add File</button>
                     <div className={this.props.errorText ? "errorText display" : "errorText"}> <p>{this.props.errorText}</p> </div>
                 </FormGroup>
             </div>
         );
     }
+
 }
 export default FormMultiFile;
--- webapp/src/components/form/MultiFileComponent.js
+++ webapp/src/components/form/MultiFileComponent.js
@@ -1,47 +1,116 @@
 import React from "react";
 import "./Style.css";
-
+import edit from '../../images/edit2.png'
+import bin from '../../images/bin.png'
 
 
 class MultiFileComponent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            lockDiscription: false,
-            description: "",
-            file: {},
+            delete: false,
+            btnSubmit: false,
+            isSubmitted: false,
+            name: null,
+            file: null,
         }
     }
 
+    // handleChange
     handleChange = event => {
         const value = event.target.value;
-   
+
         this.setState({
-            description: value
+            name: value
         })
+
     };
 
+    // handleUpload
     handleUpload = event => {
         const value = event.target.files[0];
- 
+        let fileName = this.state.name;
+
+        if (!this.state.name || this.state.name == "") {
+            fileName = value.name
+        }
+
         this.setState({
             file: value,
-            lockDiscription: true
-        })
-
-        this.props.handleUpload(value, this.state.description)
+            isSubmitted: true,
+        }, () => { this.props.handleUpload(value, fileName, this.state.delete) })
     };
+
+    // Name Change
+    nameChange = event => {
+        event.preventDefault()
+        this.setState({
+            isSubmitted: false,
+            name: "",
+            btnSubmit: true
+        })
+    }
+
+    // delete File
+    del = event => {
+        event.preventDefault()
+        this.setState({
+            delete: true
+        }, () => this.props.handleUpload(this.state.file, this.state.name, this.state.delete))
+
+    }
+
+
+    //Submit
+    submit = event => {
+        event.preventDefault()
+        this.setState({
+            btnSubmit: false,
+            isSubmitted: true
+        }, () => {
+            this.props.handleUpload(this.state.file, this.state.name, this.state.delete)
+        })
+    }
 
 
 
     render() {
+        const {
+            btnSubmit,
+            isSubmitted,
+            name,
+            errorText,
+            file
+        } = this.state;
         return (
 
             <form className="upload-item-wrapper">
+
                 <label>Upload File</label>
+
                 <div className="upload-item-container">
-                <input className={this.state.lockDiscription ? "description lock" : "description"} onChange={this.handleChange} placeholder="Please add a description" type="text"></input>
-                <input onChange={this.handleUpload} type="file"></input>
+
+                    <div className={btnSubmit ? "file-name enter" : "file-name" }>
+                        <input className={isSubmitted ? "input-field lock" : "input-field"
+                            // || this.props.errorText ? "input-field" : "input-field alert"
+                        }
+                            onChange={this.handleChange}
+                            placeholder="File Name"
+                            type="text"
+                            value={name}
+                        ></input>
+
+                        <button onClick={this.submit} className={btnSubmit ? "btn-submit show" : "btn-submit"}>Submit</button>
+                        <a onClick={this.nameChange} className={isSubmitted ? "edit show" : "edit"}><img src={edit} /></a>
+                        <a><img onClick={this.del} className={file ? "bin show" : "bin"} src={bin} /></a>
+                        <a href={this.state.file}>View File</a>
+                    </div>
+
+                    <div className={file ? "file-input-wrapper lock" : "file-input-wrapper"}>
+                        <input className={this.props.addError && !file ? "file-input error" : "file-input"} onChange={this.handleUpload} type="file"></input>
+                    </div>
+
+
                 </div>
             </form>
 
