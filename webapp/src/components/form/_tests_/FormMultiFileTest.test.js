861709d103b6d095ba11730a4ee650dc4d39b40a
Avishkar Bhoopchand <avi@avi-net.co.za>
2020-08-26 23:41:08 +0100

    Fix test

--- webapp/src/components/form/_tests_/FormMultiFileTest.test.js
+++ webapp/src/components/form/_tests_/FormMultiFileTest.test.js
@@ -20,29 +20,29 @@ describe('FormMultiFile Tests', () => {
     it('Check if FormMultiFile Page renders.', () => {
         const wrapper = shallow(<FormMultiFile />);
         expect(wrapper.length).toEqual(1);
-    })
+    });
 
     it('Check if FormMultiFile state updates in rendered().', () => {
         const wrapper = shallow(<FormMultiFile />);
-        wrapper.setState({ fileList: [{ name: null, file: null, delete: null, filePath: null }, { name: null, file: null, delete: null, filePath: null }] });
+        wrapper.setState({ fileList: [{ id: 1, name: null, file: null }, { id: 2, name: null, file: null }] });
         expect(wrapper.find('.multi-file-component').length).toEqual(2);
-    })
+    });
 
 
     it('Check if FormMultiFile adds upload components render().', () => {
         const wrapper = shallow(<FormMultiFile />);
-        wrapper.setState({ fileList: [{ name: "test", file: "test", delete: false, filePath: "1234" }, { name: "test", file: "test", delete: false, filePath: "1234" }] });
-        wrapper.instance().addFile()
+        wrapper.setState({ fileList: [{ id: 1, name: "test", file: "test" }, { id: 2, name: "test", file: "test" }] });
+        wrapper.instance().addFile();
         expect(wrapper.find('.multi-file-component').length).toEqual(2);
-    })
+    });
 
 
     it('Check if FormMultiFile adds components in render() when props change.', () => {
         const wrapper = shallow(<FormMultiFile uploadFile={uploadFile} />);
-        wrapper.setState({ fileList: [{ name: "test", file: "test", delete: false, filePath: "1234" }, { name: "test", file: "test", delete: false, filePath: "1234" }] });
-        wrapper.instance().handleUpload()
+        wrapper.setState({ fileList: [{ id: 1, name: "test", file: "test" }, { id: 2, name: "test", file: "test" }] });
+        wrapper.instance().handleUpload();
         expect(wrapper.find('.multi-file-component').length).toEqual(2);
-    })
+    });
 
 })
 
