69837d3581651e49bfcaff006cf9b207a0706042
ADMIN <admin@ADMINs-MacBook-Pro.local>
2020-09-17 21:13:03 +0200

    Adding Table Data and formatting review and answer columns as per brief

--- webapp/src/pages/ResponseList/components/ResponseListForm.js
+++ webapp/src/pages/ResponseList/components/ResponseListForm.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import '../ResponseList.css';
 import { withTranslation } from 'react-i18next';
+import { fetchResponse, fetchQuestions } from '../../../services/ResponseList/ResponseList'
+import ReactTable from 'react-table';
+import "react-table/react-table.css";
 
 
 class ResponseListForm extends Component {
@@ -8,14 +11,130 @@ class ResponseListForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            questions: [],
+            selected: [],
+            table: null,
+            toggleList: false,
+            responseTable: null
         }
+    }
+
+    componentWillMount() {
+        fetchQuestions().then(response => {
+            this.setState({
+                questions: response
+            })
+        })
+    }
+
+
+    handleSelect(question) {
+        const selected = this.state.selected;
+        let duplicate = selected.indexOf(question)
+
+        if (duplicate == -1) {
+            selected.push(question)
+        }
+        else {
+            selected.splice(duplicate, 1)
+        }
+
+        this.setState({
+            selected: selected
+        }, () => {
+            this.apiCall()
+        })
+    }
+
+    toggleList(list) {
+        this.setState({
+            toggleList: !list ? true : false
+        })
+    }
+
+    addTable(questions) {
+        this.setState({
+            table: questions
+        })
+    }
+
+
+
+    handleData() {
+        // formatAnswers
+        function formatAnswers(row, newAnswers) {
+            return row.answers = newAnswers
+        }
+        // formatReviews
+        function formatReview(row, newAnswers) {
+            return row.answers = newAnswers
+        }
+
+        fetchResponse().then(response => {
+            let handleAnwsers = [];
+            let handleReviews = [];
+
+            // Handle Anwsers
+            response.map(val => {
+                val.answers.map(answer => {
+                    handleAnwsers.push(answer.value)
+                })
+
+                val.reviewers.map(review => {
+                    review ? handleReviews.push(review.reviewer_name) : handleReviews.push("")
+                })
+
+                let formatAnwsers = formatAnswers(val, handleAnwsers);
+                let formatReviews = formatReview(val, handleReviews);
+                handleAnwsers = [];
+                handleReviews = [];
+                val.answers = formatAnwsers;
+                delete val.reviewers;
+                formatReviews.map((review, index) => {
+                    let n = "Review" + index;
+                    val[n] = review
+                })
+            })
+
+            console.log(response)
+
+
+            
+             this.setState({
+                      responseTable: response,
+                  }
+                  )
+            
+
+        })
+    }
+
+
+    generateCol() {
+        let colFormat = [];
+
+        if (this.state.responseTable) {
+            let col = Object.keys(this.state.responseTable[0]);
+            col.map(val => {
+                console.log(val)
+                colFormat.push({ id: val, Header: val, accessor: val })
+            })
+        }
+        return colFormat
     }
 
 
 
     render() {
         const t = this.props.t;
+        const {
+            questions,
+            toggleList,
+            responseTable
+        } = this.state
+
+        const columns = this.generateCol();
+
 
         return (
             <div className="container">
@@ -24,25 +143,44 @@ class ResponseListForm extends Component {
                 {/**/}
                 {/**/}
                 {/**/}
-                {/**/}
+
 
                 {/*CheckBox*/}
-                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" />
-                <label class="form-check-label" for="exampleRadios1">
+
+                <input onClick={(e) => this.handleData()} className="form-check-input" type="radio" value="" id="defaultCheck1" />
+                <label className="form-check-label" for="defaultCheck1">
                     Include un-submitted
-              </label>
+                </label>
+
 
                 {/*DropDown*/}
-                <div class="dropdown">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown button
+                <div className="container questions">
+                    <button onClick={(e) => this.toggleList(toggleList)} className="btn btn-secondary" type="button" aria-haspopup="true" aria-expanded="false">
+                        Questions
                     </button>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <a class="dropdown-item" href="#">Something else here</a>
+
+                    <div className={!toggleList ? "question-list" : "question-list show"}>
+                        {questions.length && questions.map(val => {
+                            return <div> <input onClick={(e) => this.handleSelect(val.question_id)} className="form-check-input" type="checkbox" value="" id="defaultCheck1" />
+                                <label className="form-check-label" for="defaultCheck1">
+                                    {val.type}
+                                </label>
+                            </div>
+
+                        })
+                        }
                     </div>
                 </div>
+
+
+                {/*Add Table*/}
+                <button onClick={(e) => this.addTable(questions)} type="button" className="btn btn-primary">Add Table</button>
+
+                {/* Response Table */}
+                {responseTable && <ReactTable
+                    data={responseTable}
+                    columns={columns}
+                    minRows={0} />}
 
             </div>
         )
---
+++ webapp/src/services/ResponseList/ResponseList.js
@@ -0,0 +1,62 @@
+import React from 'react'
+
+export function fetchQuestions() {
+  return new Promise((resolve, reject) => {
+    resolve([
+      { "question_id": 1, "headline": "First question", "type": "short-text" },
+      { "question_id": 2, "headline": "Second question", "type": "long-text" },
+      { "question_id": 3, "headline": "Third question", "type": "multi-choice" },
+      { "question_id": 4, "headline": "Forth question", "type": "file" },
+      { "question_id": 5, "headline": "Fifth question", "type": "single-choice" }
+    ])
+  })
+}
+
+export function fetchResponse() {
+  return new Promise((resolve, reject) => {
+    resolve(
+      [
+        {
+          "response_id": 1,
+          "user_title": "Mr",
+          "firstname": "Jimmy",
+          "lastname": "Fallon",
+          "start_date": "2020-06-01 [...]",    // ISO8601 format
+          "is_submitted": true,
+          "is_withdrawn": false,
+          "submitted_date": "2020-06-03 [...]",  // ISO8601 format
+          "language": "en",
+          "reviewers": [
+            { "reviewer_id": 23, "reviewer_name": "Mr James Dosh", "review_response_id": 33 },
+            null
+          ],
+          "answers": [
+            { "question_id": 3, "value": "Hello world", "type": "short-text", "options": null },
+            { "question_id": 5, "value": "harry-potter", "type": "multi-choice", "options": [{ "label": "Harry Potter", "value": "harry-potter" }, { "label": "X-men", "value": "x-men" }] }
+          ]
+        },
+        {
+          "response_id": 2,
+          "user_title": "Ms",
+          "firstname": "Halle",
+          "lastname": "Berry",
+          "start_date": "2020-06-01 [...]",    // ISO8601 format
+          "is_submitted": true,
+          "is_withdrawn": false,
+          "submitted_date": "2020-06-03 [...]",  // ISO8601 format
+          "language": "en",
+          "reviewers": [
+            { "reviewer_id": 63, "reviewer_name": "Mr Barack Obama", "review_response_id": 23 },
+            null
+          ],
+          "answers": [
+            { "question_id": 3, "value": "Hello world", "type": "short-text", "options": null },
+            { "question_id": 5, "value": "x-men", "type": "multi-choice", "options": [{ "label": "Harry Potter", "value": "harry-potter" }, { "label": "X-men", "value": "x-men" }] }
+          ]
+        }
+      ]
+    )
+  })
+}
+
+
