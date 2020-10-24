b3187fd84b9ba08ab5a53443dee1cd50ea82cfdf
Avishkar Bhoopchand <avi@avi-net.co.za>
2020-10-24 22:35:50 +0100

    Added tag_id

--- api/app/tags/models.py
+++ api/app/tags/models.py
@@ -21,9 +21,11 @@ class TagTranslation(db.Model):
     __tablename__ = 'tag_translation'
     __table_args__ = tuple([db.UniqueConstraint('tag_id', 'language', name='uq_tag_id_language')])
     id = db.Column(db.Integer(), primary_key=True)
+    tag_id = db.Column(db.Integer(), db.ForeignKey('tag.id'), nullable=False)
     language = db.Column(db.String(2), nullable=False)
     name = db.Column(db.String(), nullable=False)
 
-    def __init__(self, language, name):
+    def __init__(self, tag_id, language, name):
+        self.tag_id = tag_id
         self.language = language
         self.name = name
