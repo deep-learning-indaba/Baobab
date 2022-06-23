b670755042a8c25b7d9b698f316527af39ca26b8
Amrit Purshotam <amrit.purshotam@gmail.com>
2022-06-23 16:06:18 +0200

    Test retrieval multiple invoices

--- api/app/invoice/tests.py
+++ api/app/invoice/tests.py
@@ -20,23 +20,26 @@ class InvoiceApiTest(ApiTestCase):
         super().setUp()
         event = self.add_event()
         self.event_id = event.id
+        
         self.treasurer_email = "treasurer@user.com"
         treasurer = self.add_user(self.treasurer_email)
         self.treasurer_id = treasurer.id
         self.treasurer_name = treasurer.full_name
         self.add_event_role("treasurer", self.treasurer_id, event.id)
 
+        self.applicant_email = "applicant@user.com"
+        self.applicant = self.add_user(self.applicant_email)
+        self.applicant_id = self.applicant.id
+
     def test_get_invoice_success(self):
-        applicant_email = "applicant@user.com"
-        applicant = self.add_user(applicant_email)
-        offer = self.add_offer(applicant.id, self.event_id)
+        offer = self.add_offer(self.applicant_id, self.event_id)
         offer_id = offer.id
         line_items = self.get_default_line_items()
-        invoice = self.add_invoice(self.treasurer_id, applicant.id, line_items, applicant_email)
+        invoice = self.add_invoice(self.treasurer_id, self.applicant_id, line_items, self.applicant_email)
         invoice_id = invoice.id
         self.add_invoice_payment_intent(invoice_id)
         self.add_offer_invoice(invoice_id, offer.id)
-        header = self.get_auth_header_for(applicant_email)
+        header = self.get_auth_header_for(self.applicant_email)
 
         params = {'invoice_id': invoice_id}
         response = self.app.get(self.url, headers=header, data=params)
@@ -44,8 +47,8 @@ class InvoiceApiTest(ApiTestCase):
         data = json.loads(response.data)
         self.assertEqual(response.status_code, 200)
         
-        self.assertEqual(data["customer_email"], applicant_email)
-        self.assertEqual(data["client_reference_id"], str(applicant.id))
+        self.assertEqual(data["customer_email"], self.applicant_email)
+        self.assertEqual(data["client_reference_id"], str(self.applicant_id))
         self.assertEqual(data["iso_currency_code"], 'usd')
         self.assertEqual(data["created_by_user_id"], self.treasurer_id)
         self.assertEqual(data["created_by_user"], self.treasurer_name)
@@ -74,10 +77,8 @@ class InvoiceApiTest(ApiTestCase):
         self.assertEqual(data["invoice_payment_intents"][0]["has_session_expired"], False)
 
     def test_prevent_retrieval_of_someone_elses_invoice(self):
-        applicant_email = "applicant@user.com"
-        applicant = self.add_user("applicant@user.com")
         line_items = self.get_default_line_items()
-        invoice = self.add_invoice(self.treasurer_id, applicant.id, line_items, applicant_email)
+        invoice = self.add_invoice(self.treasurer_id, self.applicant_id, line_items, self.applicant_email)
         invoice_id = invoice.id
         another_applicant_email = "another_applicant@user.com"
         self.add_user(another_applicant_email)
@@ -89,23 +90,19 @@ class InvoiceApiTest(ApiTestCase):
         self.assertEqual(response.status_code, INVOICE_NOT_FOUND[1])
     
     def test_cancel_own_invoice(self):
-        applicant_email = "applicant@user.com"
-        applicant = self.add_user(applicant_email)
         line_items = self.get_default_line_items()
-        invoice = self.add_invoice(self.treasurer_id, applicant.id, line_items, applicant_email)
+        invoice = self.add_invoice(self.treasurer_id, self.applicant_id, line_items, self.applicant_email)
         invoice_id = invoice.id
 
-        header = self.get_auth_header_for(applicant_email)
+        header = self.get_auth_header_for(self.applicant_email)
         params = {'invoice_id': invoice_id}
         response = self.app.delete(self.url, headers=header, data=params)
 
         self.assertEqual(response.status_code, 200)
     
     def test_prevent_canceling_someone_else_invoices(self):
-        applicant_email = "applicant@user.com"
-        applicant = self.add_user("applicant@user.com")
         line_items = self.get_default_line_items()
-        invoice = self.add_invoice(self.treasurer_id, applicant.id, line_items, applicant_email)
+        invoice = self.add_invoice(self.treasurer_id, self.applicant_id, line_items, self.applicant_email)
         invoice_id = invoice.id
         another_applicant_email = "another_applicant@user.com"
         self.add_user(another_applicant_email)
@@ -117,15 +114,13 @@ class InvoiceApiTest(ApiTestCase):
         self.assertEqual(response.status_code, INVOICE_NOT_FOUND[1])
 
     def test_prevent_cancel_of_already_canceled_invoice(self):
-        applicant_email = "applicant@user.com"
-        applicant = self.add_user(applicant_email)
         line_items = self.get_default_line_items()
-        invoice = self.add_invoice(self.treasurer_id, applicant.id, line_items, applicant_email)
+        invoice = self.add_invoice(self.treasurer_id, self.applicant_id, line_items, self.applicant_email)
         invoice_id = invoice.id
-        invoice.cancel(applicant.id)
+        invoice.cancel(self.applicant_id)
         db.session.commit()
 
-        header = self.get_auth_header_for(applicant_email)
+        header = self.get_auth_header_for(self.applicant_email)
         params = {'invoice_id': invoice_id}
         response = self.app.delete(self.url, headers=header, data=params)
 
@@ -134,16 +129,14 @@ class InvoiceApiTest(ApiTestCase):
         self.assertEqual(data["message"], "Invoice has already been canceled.")
 
     def test_prevent_cancel_of_already_paid_invoice(self):
-        applicant_email = "applicant@user.com"
-        applicant = self.add_user(applicant_email)
         line_items = self.get_default_line_items()
-        invoice = self.add_invoice(self.treasurer_id, applicant.id, line_items, applicant_email)
+        invoice = self.add_invoice(self.treasurer_id, self.applicant_id, line_items, self.applicant_email)
         invoice_id = invoice.id
         invoice_payment_status = InvoicePaymentStatus.from_stripe_webhook(PaymentStatus.PAID, time())
         invoice.invoice_payment_statuses.append(invoice_payment_status)
         db.session.commit()
 
-        header = self.get_auth_header_for(applicant_email)
+        header = self.get_auth_header_for(self.applicant_email)
         params = {'invoice_id': invoice_id}
         response = self.app.delete(self.url, headers=header, data=params)
 
@@ -187,4 +180,39 @@ class InvoiceListApiTest(ApiTestCase):
         self.assertEqual(data, [])
 
     def test_get_with_multiple_invoices(self):
-        pass
+        line_items_1 = self.get_default_line_items()
+        invoice_1 = self.add_invoice(self.treasurer_id, self.applicant_id, line_items_1, self.applicant_email)
+        invoice_1_id = invoice_1.id
+        invoice_payment_status_1 = InvoicePaymentStatus.from_stripe_webhook(PaymentStatus.PAID, time())
+        invoice_1.invoice_payment_statuses.append(invoice_payment_status_1)
+        line_items_2 = self.get_default_line_items()
+        line_items_2[0].amount = 49.99
+        line_items_2[1].amount = 39.99
+        invoice_2 = self.add_invoice(self.treasurer_id, self.applicant_id, line_items_2, self.applicant_email)
+        invoice_2_id = invoice_2.id
+        db.session.commit()
+
+        header = self.get_auth_header_for(self.applicant_email)
+        response = self.app.get(self.url, headers=header)
+
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(data), 2)
+
+        self.assertEqual(data[0]["customer_email"], self.applicant_email)
+        self.assertEqual(data[0]["client_reference_id"], str(self.applicant_id))
+        self.assertEqual(data[0]["iso_currency_code"], 'usd')
+        self.assertEqual(data[0]["created_by_user_id"], self.treasurer_id)
+        self.assertEqual(data[0]["created_by_user"], self.treasurer_name)
+        self.assertIsNotNone(data[0]["created_at"])
+        self.assertEqual(data[0]["total_amount"], 299.98)
+        self.assertEqual(data[0]["current_payment_status"], PaymentStatus.PAID.value)
+
+        self.assertEqual(data[1]["customer_email"], self.applicant_email)
+        self.assertEqual(data[1]["client_reference_id"], str(self.applicant_id))
+        self.assertEqual(data[1]["iso_currency_code"], 'usd')
+        self.assertEqual(data[1]["created_by_user_id"], self.treasurer_id)
+        self.assertEqual(data[1]["created_by_user"], self.treasurer_name)
+        self.assertIsNotNone(data[1]["created_at"])
+        self.assertEqual(data[1]["total_amount"], 89.98)
+        self.assertEqual(data[1]["current_payment_status"], PaymentStatus.UNPAID.value)
