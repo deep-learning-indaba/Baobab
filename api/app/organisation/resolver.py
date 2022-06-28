1baebce37ae6fa3a3c92b0b13097995e6fc2765b
Amrit Purshotam <amrit.purshotam@gmail.com>
2022-06-28 13:06:43 +0200

    Use previous org resolution
    Fixed detached instance issue

--- api/app/organisation/resolver.py
+++ api/app/organisation/resolver.py
@@ -6,22 +6,18 @@ from werkzeug.exceptions import BadRequest
 
 class OrganisationResolver():
     _cache = None
-    _secrets_cache = None
 
     @classmethod
     def _populate_cache(cls):
         LOGGER.info('Populating Organisation Cache')
         organisations = OrganisationRepository.get_all()
         cls._cache = {}
-        cls._secrets_cache = {}
         for org in organisations:
             cls._cache[org.domain] = org
-            cls._secrets_cache[org.domain] = org.stripe_webhook_secret_key
 
     @classmethod
     def reset_cache(cls):
         cls._cache = None
-        cls._secrets_cache = None
 
     @classmethod
     def bust_cache(cls):
@@ -29,7 +25,7 @@ class OrganisationResolver():
 
     @classmethod
     def resolve_from_domain(cls, domain):
-        if not cls._cache or not cls._secrets_cache:
+        if not cls._cache:
             cls._populate_cache()
         
         if domain not in cls._cache:
@@ -45,13 +41,14 @@ class OrganisationResolver():
     
     @classmethod
     def resolve_from_stripe_signature(cls, signed_payload: str, expected_signature: str):
-        if not cls._cache or not cls._secrets_cache:
+        if not cls._cache:
             cls._populate_cache()
         
-        for org_domain, secret in cls._secrets_cache.items():
+        for org in cls._cache.values():
+            secret = org.stripe_webhook_secret_key
             if secret:
                 if verify_payload(signed_payload, secret, expected_signature):
-                    return cls._cache[org_domain]
+                    return org
         
         raise BadRequest('Could not resolve organisation from stripe signature')
     
