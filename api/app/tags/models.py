aef4fa403ae72fe44a47cc7d156e85c125397a15
Daniela Massiceti <daniela.massiceti@gmail.com>
2023-03-26 07:27:37 +0100

    added type and description to tag and tagtranslation models

--- api/app/tags/models.py
+++ api/app/tags/models.py
@@ -1,21 +1,30 @@
 from datetime import datetime
 from app import db
 from app.events.models import Event
+from enum import Enum
+
+class TagType(Enum):
+    REGISTERED = 'registered'
 
 class Tag(db.Model):
     __tablename__ = 'tag'
     id = db.Column(db.Integer(), primary_key=True)
     event_id = db.Column(db.Integer(), db.ForeignKey('event.id'), nullable=False)
+    type = db.Column(db.Enum(TagType), nullable=True)
 
     translations = db.relationship('TagTranslation', lazy='dynamic')
 
-    def __init__(self, event_id):
+    def __init__(
+            self, 
+            event_id,
+            type
+        ):
         self.event_id = event_id
+        self.type = type
     
     def get_translation(self, language):
         translation = self.translations.filter_by(language=language).first()
         return translation
-
 
 class TagTranslation(db.Model):
     __tablename__ = 'tag_translation'
@@ -24,8 +33,10 @@ class TagTranslation(db.Model):
     tag_id = db.Column(db.Integer(), db.ForeignKey('tag.id'), nullable=False)
     language = db.Column(db.String(2), nullable=False)
     name = db.Column(db.String(), nullable=False)
+    description = db.Column(db.String(), nullable=True)
 
-    def __init__(self, tag_id, language, name):
+    def __init__(self, tag_id, language, name, description=None):
         self.tag_id = tag_id
         self.language = language
         self.name = name
+        self.description = description
