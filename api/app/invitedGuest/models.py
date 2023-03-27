bffad7bcd75186594251ce757af4ff9997017398
Daniela Massiceti <daniela.massiceti@gmail.com>
2023-03-27 09:51:34 +0100

    wip - adding tags to offers and invited guests

--- api/app/invitedGuest/models.py
+++ api/app/invitedGuest/models.py
@@ -1,6 +1,5 @@
-
 from app import db
-
+from app.tags.models import Tag
 
 class InvitedGuest(db.Model):
     __tablename__ = "invited_guest"
@@ -11,11 +10,25 @@ class InvitedGuest(db.Model):
     user_id = db.Column(db.Integer(), db.ForeignKey(
         "app_user.id"), nullable=False)
     role = db.Column(db.String(50), nullable=False)
+    invited_guest_tags = db.relationship('InvitedGuestTag')
+
 
     def __init__(self, event_id, user_id, role):
         self.event_id = event_id
         self.user_id = user_id
         self.role = role
+
+class InvitedGuestTag(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('app_user.id'), nullable=False)
+    tag_id = db.Column(db.Integer(), db.ForeignKey('tag.id'), nullable=False)
+
+    invited_guest = db.relationship('InvitedGuest', foreign_keys=[user_id])
+    tag = db.relationship('Tag', foreign_keys=[tag_id])
+
+    def __init__(self, user_id, tag_id):
+        self.user_id = user_id
+        self.tag_id = tag_id
 
 
 class GuestRegistration(db.Model):
--- api/app/registration/models.py
+++ api/app/registration/models.py
@@ -1,8 +1,9 @@
-from app import db
-from app.invoice.models import PaymentStatus
 from datetime import datetime, date, time
 from sqlalchemy import func
 
+from app import db
+from app.invoice.models import PaymentStatus
+from app.tags.models import Tag
 
 class Offer(db.Model):
 
@@ -26,6 +27,7 @@ class Offer(db.Model):
     payment_amount = db.Column(db.String(), nullable=True)
 
     user = db.relationship('AppUser', foreign_keys=[user_id])
+    #offer_tags = db.relationship('OfferTag')
     offer_invoices = db.relationship('OfferInvoice')
 
     def is_expired(self):
@@ -42,6 +44,18 @@ class Offer(db.Model):
             if offer_invoice.invoice.current_payment_status.payment_status in valid_payment_statuses:
                 return True
         return False
+
+""" class OfferTag(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('app_user.id'), nullable=False)
+    tag_id = db.Column(db.Integer(), db.ForeignKey('tag.id'), nullable=False)
+
+    offer = db.relationship('Offer', foreign_keys=[user_id])
+    tag = db.relationship('Tag', foreign_keys=[tag_id])
+
+    def __init__(self, user_id, tag_id):
+        self.user_id = user_id
+        self.tag_id = tag_id """
 
 class RegistrationForm(db.Model):
 
