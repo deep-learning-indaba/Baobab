f6574ce6c1f37781a48b498fd4ce479447c56a5f
Daniela Massiceti <daniela.massiceti@gmail.com>
2023-04-03 02:18:10 +0100

    adding tag config functionality wip

--- webapp/src/pages/tagConfig/components/TagConfigComponent.js
+++ webapp/src/pages/tagConfig/components/TagConfigComponent.js
@@ -4,7 +4,10 @@ import { Link } from "react-router-dom";
 import { withRouter } from "react-router";
 import { withTranslation } from 'react-i18next';
 import FormTextBox from "../../../components/form/FormTextBox";
+import FormTextArea from "../../../components/form/FormTextArea";
 import FormSelect from "../../../components/form/FormSelect";
+
+const TAG_TYPES = ["RESPONSE", "REGISTRATION"];
 
 class TagConfigComponent extends Component {
   constructor(props) {
@@ -12,15 +15,20 @@ class TagConfigComponent extends Component {
 
     this.state = {
       tags: [],
+      updatedTag: {
+        name: {},
+        tag_type: "",
+        description: {},
+        language: ""
+      },
       isMultiLingual: this.props.organisation.languages.length > 1,
       isValid: false,
       loading: false,
       error: "",
       errors: [],
-      showErrors: false
+      showErrors: false,
+      tagEntryVisible: false
     };
-
-    console.log(this.state);
   }
 
   componentDidMount() {
@@ -31,6 +39,7 @@ class TagConfigComponent extends Component {
           tags: result.tags,
           error: result.error
         });
+        console.log(this.state.tags);
       });
     }
   }
@@ -38,8 +47,7 @@ class TagConfigComponent extends Component {
   onClickAdd = () => {
     const errors = []; //TDDO, validate tag
     if (errors.length === 0) {
-      const tag = null; //TODO
-      tagsService.addTag(tag).then(result => {
+      tagsService.addTag(this.state.updatedTag).then(result => {
         if (result.error) {
           this.setState({
             errors: [this.props.t(result.error)],
@@ -60,8 +68,7 @@ class TagConfigComponent extends Component {
   onClickUpdate = () => {
     const errors = []; //TODO, validate tag
     if (errors.length === 0) {
-      const tag = null; //TODO
-      tagsService.updateTag(tag).then(result => {
+      tagsService.updateTag(this.state.updatedTag).then(result => {
         if (result.error) {
           this.setState({
             errors: [this.props.t(result.error)],
@@ -118,27 +125,166 @@ class TagConfigComponent extends Component {
   };
 
   updateTextField = (fieldName, e, lang) => {
-    //TODO
+    let u;
+    if (lang) {
+      u = {
+        ...this.state.updatedTag,
+        [fieldName]: {
+          ...this.state.updatedTag[fieldName],
+          [lang]: e.target.value
+        }
+      };
+    }
+    else {
+      u = {
+        ...this.state.updatedTag,
+        [fieldName]: e.target.value
+      };
+    }
+    this.updateState(u);
   };
 
   updateDropDown = (fieldName, dropdown) => {
-    //TODO
+    const u = {
+      ...this.state.updatedTag,
+      [fieldName]: dropdown.value
+    };
+    this.updateState(u);
   };
 
-  updateState = (tags) => {
-    //TODO
+  updateState = (tag) => {
+    this.setState({
+      updatedTag: tag
+    }, () => {
+
+      const errors = [];//this.validateEventDetails();
+
+      this.setState({
+        //allFieldsComplete: this.areAllFieldsComplete(),
+        errors: errors,
+        isValid: errors.length === 0
+      });
+    });
   }
 
   renderTagTable = () => {
     const tagRows = [];
+    const t = this.props.t;
+    const tags = this.state.tags;
+    tagRows.push(<h3>{t("Tags")}</h3>)
+    for (let i = 0; i < tags.length; i++) {
+      const tag = tags[i];
+      tagRows.push(
+        <div className="tag">
+          <div className="tag-info">
+            {tag.name}
+          </div>
+        </div>
+      );
+    }
+
     //TODO
+    return tagRows
     return <div></div>;
+  }
+
+  renderTagEntry = () => {
+    const t = this.props.t;
+    const tagEntryForm = [];
+    tagEntryForm.push(
+      <div className={"form-group row"}>
+        <label
+          className={"col-sm-2 col-form-label"}
+          htmlFor={"tag_type"}>
+          <span className="required-indicator">*</span>
+          {t("Tag Type")}
+        </label>
+        <div className="col-sm-10">
+          <FormSelect
+            id={"tag_type"}
+            name={"tag_type"}
+            required={true}
+            defaultValue={this.state.updatedTag.tag_type || null}
+            onChange={this.updateDropDown}
+            options={
+              TAG_TYPES.map((tagType) => ({
+                value: tagType,
+                label: t(tagType)
+              }))
+            }
+          />
+        </div>
+      </div>
+    );
+    tagEntryForm.push(
+    this.props.organisation.languages.map((lang) => (
+      <div>
+      <div className={"form-group row"} key={"name_div"+lang.code}>
+        <label
+        className={"col-sm-2 col-form-label"} 
+        htmlFor={"name_" + lang.code}>
+        <span className="required-indicator">*</span>
+        {this.state.isMultiLingual ? t(this.getFieldNameWithLanguage("Tag Name", lang.description)) : t("Tag Name")}
+        </label>
+        <div className="col-sm-10">
+          <FormTextBox
+          id={"name_" + lang.code}
+          name={"name_" + lang.code}
+          type="text"
+          placeholder={this.state.isMultiLingual ? t(this.getFieldNameWithLanguage("Name of the tag", lang.description)) : t("Name of the tag")}
+          required={true}
+          onChange={e => this.updateTextField("name", e, lang.code)}
+          value={this.state.updatedTag.name[lang.code]}
+        />
+        </div>
+      </div>
+      <div className={"form-group row"} key={"description_div"+lang.code}>
+        <label
+          className={"col-sm-2 col-form-label"}
+          htmlFor={"description"}>
+          {this.state.isMultiLingual ? t(this.getFieldNameWithLanguage("Tag Name", lang.description)) : t("Tag Description")}
+        </label>
+        <div className="col-sm-10">
+          <FormTextArea
+            id={"description"}
+            name={"description"}
+            type="text"
+            placeholder={t("Description of the tag")}
+            required={false}
+            onChange={e => this.updateTextField("description", e, lang.code)}
+            value={this.state.updatedTag.description[lang.code]}
+          />
+        </div>
+      </div>
+    </div>
+    )));
+
+    tagEntryForm.push(
+      <div className={"form-group row"}>
+        <button className="btn btn-primary" onClick={this.onClickAdd()}>
+          {t("Save")}
+        </button>
+      </div>
+    );
+    console.log(this.state);
+    return tagEntryForm;
+  }
+
+  setTagEntryVisible = () => {
+    this.setState({
+      tagEntryVisible: true
+      });
+  }
+
+  getFieldNameWithLanguage = (input, lang) => {
+    return input + " in " + lang;
   }
 
   render() {
     const {
       loading,
       error,
+      tagEntryVisible
     } = this.state;
 
     const loadingStyle = {
@@ -165,12 +311,18 @@ class TagConfigComponent extends Component {
     }
 
     const t = this.props.t;
+    //console.log(this.props.user);
+    //console.log(this.props.user.is_admin);
 
     return (
       <div>
         <div className="card">
           <form>
           {this.renderTagTable()}
+          {
+            <a onClick={this.setTagEntryVisible} id="new_tag_button" name="new_tag_button" className="btn btn-primary">{t("New Tag")}</a>
+          }
+          {tagEntryVisible && this.renderTagEntry()}
           {this.state.showErrors && this.getErrorMessages(this.state.errors)}
           </form>
         </div>
