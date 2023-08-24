180610d88d8cc4f3b74b5e90c2b3b520481e512f
lucas-meyer-7 <lucas.meyer77@gmail.com>
2023-08-24 21:50:48 +0200

    Fixed migration

--- api/migrations/versions/6d2e1dc65ef6_add_is_private_to_review_response.py
+++ api/migrations/versions/6d2e1dc65ef6_add_is_private_to_review_response.py
@@ -3,18 +3,19 @@ Adds a field called ``is_private`` to review_response table.
 It is used to specify whether the response from the reviewer is public,
 or private. If it is private, then only the meta-reviewer can see the response.
 
-Revision ID: e23b638b03cc
-Revises: 9143756e596d
-Create Date: 2023-07-30 19:16:28.509240
+Revision ID: 6d2e1dc65ef6
+Revises: 5d5b0524b4fa
+Create Date: 2023-08-24 19:44:58.051586
 
 """
 
 # revision identifiers, used by Alembic.
-revision = 'e23b638b03cc'
-down_revision = '9143756e596d'
+revision = '6d2e1dc65ef6'
+down_revision = '5d5b0524b4fa'
 
 from alembic import op
 import sqlalchemy as sa
+
 
 def upgrade():
     op.add_column('review_response', sa.Column('is_private', sa.Boolean(), nullable=True))
