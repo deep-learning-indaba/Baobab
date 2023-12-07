d01bc1ea2e024f46bbf8c6b2a4a14b70c71143ce
Avishkar Bhoopchand <avi@avi-net.co.za>
2023-12-07 11:35:36 +0200

    Added application response report api

---
+++ api/app/reporting/api.py
@@ -0,0 +1,36 @@
+from datetime import date, datetime
+from app.utils.language import translatable
+from app.reporting.repository import ReportingRepository
+from flask_restful import fields, marshal
+import flask_restful as restful
+
+from app.utils.auth import event_admin_required
+from app.applicationModel.repository import ApplicationFormRepository
+from app.utils import errors
+
+def response_info(response):
+    return {
+        'user_id': response.AppUser.id,
+        'email': response.AppUser.email,
+        'firstname': response.AppUser.firstname,
+        'lastname': response.AppUser.lastname,
+        'response_id': response.Response.id,
+        'response_language': response.Response.language,
+        'section_id': response.Section.id,
+        'section': response.SectionTranslation.name,
+        'question_id': response.Question.id,
+        'question': response.QuestionTranslation.headline,
+        'answer': response.Answer.value,
+    }
+
+class ApplicationResponseReportAPI(restful.Resource):
+
+    @event_admin_required
+    @translatable
+    def get(self, event_id: int, language: str):
+        app_form = ApplicationFormRepository.get_by_event_id(event_id)
+        if not app_form:
+            return errors.APPLICATION_FORM_NOT_FOUND
+        responses = ReportingRepository.get_applications_for_form(app_form.id, language)
+
+        return [response_info(response) for response in responses]
