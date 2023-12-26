ca175178425c8b83241f05c55ad35bec53384d92
Avishkar Bhoopchand <avi@avi-net.co.za>
2023-12-26 22:14:24 +0000

    Add reviews report

--- api/app/reporting/api.py
+++ api/app/reporting/api.py
@@ -23,6 +23,25 @@ def response_info(response):
         'answer': response.Answer.value,
     }
 
+def review_info(review):
+    return {
+        'user_id': review.applicant_id,
+        'email': review.applicant_email,
+        'firstname': review.applicant_firstname,
+        'lastname': review.applicant_lastname,
+        'reviewer_user_id': review.reviewer_id,
+        'reviewer_email': review.reviewer_email,
+        'reviewer_firstname': review.reviewer_firstname,
+        'reviewer_lastname': review.reviewer_lastname,
+        'review_id': review.ReviewResponse.id,
+        'review_language': review.ReviewResponse.language,
+        'section_id': review.ReviewSectionTranslation.review_section_id,
+        'section': review.ReviewSectionTranslation.headline,
+        'question_id': review.ReviewQuestionTranslation.review_question_id,
+        'question': review.ReviewQuestionTranslation.headline,
+        'score': review.ReviewScore.value,
+    }
+
 class ApplicationResponseReportAPI(restful.Resource):
 
     @event_admin_required
@@ -34,3 +53,14 @@ class ApplicationResponseReportAPI(restful.Resource):
         responses = ReportingRepository.get_applications_for_form(app_form.id, language)
 
         return [response_info(response) for response in responses]
+
+class ReviewReportAPI(restful.Resource):
+
+    @event_admin_required
+    @translatable
+    def get(self, event_id: int, language: str):
+        app_form = ApplicationFormRepository.get_by_event_id(event_id)
+        if not app_form:
+            return errors.APPLICATION_FORM_NOT_FOUND
+        reviews = ReportingRepository.get_reviews_for_form(app_form.id, language)
+        return [review_info(review) for review in reviews]
