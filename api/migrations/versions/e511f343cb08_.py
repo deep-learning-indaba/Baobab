cb1b39b228a53b8005a161a2f531f36ce2cc0707
Amrit Purshotam <amrit.purshotam@gmail.com>
2024-04-02 21:55:42 +0200

    Complete removal of continous journal

---
+++ api/migrations/versions/e511f343cb08_.py
@@ -0,0 +1,27 @@
+"""empty message
+
+Revision ID: e511f343cb08
+Revises: 81526c9dbb6f
+Create Date: 2024-04-02 19:49:24.440649
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'e511f343cb08'
+down_revision = '81526c9dbb6f'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.execute("""DELETE FROM pg_enum
+        WHERE enumlabel = 'CONTINUOUS_JOURNAL' 
+        AND enumtypid = (
+        SELECT oid FROM pg_type WHERE typname = 'event_type'
+        )""")
+
+
+def downgrade():
+    op.execute("COMMIT")
+    op.execute("ALTER TYPE event_type ADD VALUE 'CONTINUOUS_JOURNAL'")
