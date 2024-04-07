b2f97545068525e7d7251e835078ae0439aadd2d
Avishkar Bhoopchand <avi@avi-net.co.za>
2024-04-07 13:57:57 +0100

    Add merge migration

---
+++ api/migrations/versions/c1871115c32d_merge.py
@@ -0,0 +1,22 @@
+"""Merge migration
+
+Revision ID: c1871115c32d
+Revises: ('062dc67db351', 'e511f343cb08')
+Create Date: 2024-04-07 12:57:26.147043
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'c1871115c32d'
+down_revision = ('062dc67db351', 'e511f343cb08')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
