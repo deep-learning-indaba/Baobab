25ac768c8095ee57595e8217b65182de1e23de7d
Yvan CARRE <kvilmorin@gmail.com>
2025-01-29 01:45:24 +0200

    Outcome model adjustment

--- api/app/outcome/api.py
+++ api/app/outcome/api.py
@@ -15,6 +15,7 @@ from app import LOGGER
 from app import db
 from app.utils import errors
 from app.utils import misc
+from app.responses.repository import ResponseRepository as response_repository
 
 
 def _extract_status(outcome):
@@ -36,14 +37,35 @@ user_fields = {
     'user_title': fields.String
 }
 
+
+answer_fields = {
+    'id': fields.Integer,
+    'question_id': fields.Integer,
+    'question': fields.String(attribute='question.headline'),
+    'value': fields.String(attribute='value_display'),
+    'question_type': fields.String(attribute='question.type')
+}
+
+response_fields = {
+    'id': fields.Integer,
+    'application_form_id': fields.Integer,
+    'user_id': fields.Integer,
+    'is_submitted': fields.Boolean,
+    'submitted_timestamp': fields.DateTime(dt_format='iso8601'),
+    'is_withdrawn': fields.Boolean,
+    'withdrawn_timestamp': fields.DateTime(dt_format='iso8601'),
+    'started_timestamp': fields.DateTime(dt_format='iso8601'),
+    'answers': fields.List(fields.Nested(answer_fields))
+}
+
 outcome_list_fields = {
     'id': fields.Integer,
     'status': fields.String(attribute=_extract_status),
     'timestamp': fields.DateTime(dt_format='iso8601'),
     'user': fields.Nested(user_fields),
-    'updated_by_user': fields.Nested(user_fields)
+    'updated_by_user': fields.Nested(user_fields),
+    'response': fields.Nested(response_fields),
 }
-
 
 class OutcomeAPI(restful.Resource):
     @event_admin_required
@@ -51,12 +73,14 @@ class OutcomeAPI(restful.Resource):
     def get(self, event_id):
         req_parser = reqparse.RequestParser()
         req_parser.add_argument('user_id', type=int, required=True)
+        req_parser.add_argument('response_id', type=int, required=True)
         args = req_parser.parse_args()
 
         user_id = args['user_id']
+        response_id=args['response_id']
 
         try:
-            outcome = outcome_repository.get_latest_by_user_for_event(user_id, event_id)
+            outcome = outcome_repository.get_latest_by_user_for_event(user_id, event_id,response_id)
             if not outcome:
                 return errors.OUTCOME_NOT_FOUND
             
@@ -75,6 +99,7 @@ class OutcomeAPI(restful.Resource):
         req_parser = reqparse.RequestParser()
         req_parser.add_argument('user_id', type=int, required=True)
         req_parser.add_argument('outcome', type=str, required=True)
+        req_parser.add_argument('response_id', type=int, required=True)
         args = req_parser.parse_args()
 
         event = event_repository.get_by_id(event_id)
@@ -84,6 +109,10 @@ class OutcomeAPI(restful.Resource):
         user = user_repository.get_by_id(args['user_id'])
         if not user:
             return errors.USER_NOT_FOUND
+        
+        response = response_repository.get_by_id_and_user_id(args['response_id'],args['user_id'])
+        if not response:
+            return errors.RESPONSE_NOT_FOUND
 
         try:
             status = Status[args['outcome']]
@@ -92,7 +121,7 @@ class OutcomeAPI(restful.Resource):
 
         try:
             # Set existing outcomes to no longer be the latest outcome
-            existing_outcomes = outcome_repository.get_all_by_user_for_event(args['user_id'], event_id)
+            existing_outcomes = outcome_repository.get_all_by_user_for_event(args['user_id'], event_id, args['response_id'])
             for existing_outcome in existing_outcomes:
                 existing_outcome.reset_latest()
 
@@ -101,7 +130,8 @@ class OutcomeAPI(restful.Resource):
                     event_id,
                     args['user_id'],
                     status,
-                    g.current_user['id'])
+                    g.current_user['id'],
+                    args['response_id'])
 
             outcome_repository.add(outcome)
             db.session.commit()
