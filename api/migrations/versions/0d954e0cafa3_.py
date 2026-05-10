f335c3c61ee75c56641188335eebfe7b516980e3
Avishkar Bhoopchand <avi@avi-net.co.za>
2026-05-10 16:52:18 +0100

    Fix migration branches

--- api/migrations/versions/0d954e0cafa3_.py
+++ api/migrations/versions/0d954e0cafa3_.py
@@ -1,14 +1,14 @@
 """empty message
 
 Revision ID: 0d954e0cafa3
-Revises: ('add_form_type_stage', 'b2c3d4e5f6a7')
+Revises: b2c3d4e5f6a7
 Create Date: 2026-05-04 17:04:22.183789
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '0d954e0cafa3'
-down_revision = ('add_form_type_stage', 'b2c3d4e5f6a7')
+down_revision = 'b2c3d4e5f6a7'
 
 from alembic import op
 import sqlalchemy as sa
--- api/migrations/versions/b2c3d4e5f6a7_add_form_response_tag.py
+++ api/migrations/versions/b2c3d4e5f6a7_add_form_response_tag.py
@@ -1,14 +1,14 @@
 """Add form_response_tag table
 
 Revision ID: b2c3d4e5f6a7
-Revises: a1b2c3d4e5f6
+Revises: ('a1b2c3d4e5f6', 'add_form_type_stage')
 Create Date: 2026-05-04 00:00:00.000000
 
 """
 
 # revision identifiers, used by Alembic.
 revision = 'b2c3d4e5f6a7'
-down_revision = 'a1b2c3d4e5f6'
+down_revision = ('a1b2c3d4e5f6', 'add_form_type_stage')
 
 from alembic import op
 import sqlalchemy as sa
